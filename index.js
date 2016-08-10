ca3a58e89682a7349bf5193d889f128eec28dd5a
Jon High <jon.highmu@gmail.com>
2016-08-10 18:28:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,49 @@
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+var number = 10
+
+function add5() {
+  number += 5
+}
+
+function divideBy3() {
+  number /= 3
+}
+
+function inc(n) {
+  return n += 1
+}
+
+function dec(n) {
+  return n -= 1
+}
+
+function makeInt(n) {
+  return parseInt()
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n){
+  return parseFloat()
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
