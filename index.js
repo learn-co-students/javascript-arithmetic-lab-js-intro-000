2f4db2786687aba13eda23ca2aded949fae3acdb
David Ganthier <david.ganthier@gmail.com>
2016-08-13 18:29:19 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function inc(n) {
+  parseInt(n + 1);
+  return n + 1;
+}
+
+function dec(n) {
+  parseFloat(n - 1);
+  return n - 1;
+}
+
+function makeInt(n) {
+  parseInt(n);
+  return n;
+}
+
+//makeInt(n) = 10;
