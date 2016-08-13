72cd085e8cb49446abcc2f5254a62493706d8da7
Amanda Hooge <amandolin@ymail.com>
2016-08-13 13:32:47 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b){
+  return a+b;
+}
+
+function subtract(a, b){
+  return a-b;
+}
+
+function multiply(a, b){
+  return a*b;
+}
+
+function divide(a,b){
+  return a/b;
+}
+
+function inc(n){
+  return ++n;
+}
+
+function dec(a){
+  return --a;
+}
+
+function makeInt(string){
+  return parseInt(string, 10);
+}
+
+function preserveDecimal(n){
+  return parseFloat(n);
+}
