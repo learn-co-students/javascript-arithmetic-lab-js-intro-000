fe7dda56732f9aab6f3e81ba6fddebc78cd39a66
Smit Patel <smitp184@gmail.com>
2016-09-03 16:23:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+var add = function(a,b) {
+  return a+b;
+}
+var subtract = function(a,b) {
+  return a-b;
+}
+var multiply = function(a,b) {
+  return a*b;
+}
+var divide = function(a,b) {
+  return a/b;
+}
+var inc = function(n) {
+   return n + 1;
+}
+var dec = function(n) {
+   return n - 1;
+}
+var makeInt = function(a, n) {
+  return parseInt(a, 10);
+}
+var preserveDecimal = function(n) {
+  return parseFloat(n);
+}
