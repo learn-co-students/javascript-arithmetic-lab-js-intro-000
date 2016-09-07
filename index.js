5adbe43dd15fd554d6ace4b69f04287b409b5601
David Guttag <dguttag@gmail.com>
2016-09-07 02:57:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a, b) {
+  var answer = a + b;
+  return answer
+}
+
+function subtract(a, b) {
+  var answer = a - b;
+  return answer
+}
+
+function multiply(a, b) {
+  var answer = a * b
+  return answer
+}
+
+function divide(a, b) {
+  var answer = a / b
+  return answer
+}
+
+function inc(a) {
+  var answer = a
+  answer++
+  return answer
+}
+
+function dec(a) {
+  var answer = a
+  answer--
+  return answer
+ }
+
+function makeInt(a) {
+  return parseInt(a, 10)
+}
+
+function preserveDecimal(a) {
+  return parseFloat(a)
+}
