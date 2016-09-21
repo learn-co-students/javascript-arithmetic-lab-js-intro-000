fe6e426b8c1ee574f8a321a3c409cb810e5a628c
Debora Ermel <debbyermel@gmail.com>
2016-09-21 08:19:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,64 @@
+
+
+function add(a,b) {
+
+return `${a + b}`;
+
+}
+add(440,517);
+
+
+
+function subtract(a,b) {
+
+return `${a - b}`;
+
+}
+subtract(4,51);
+
+
+
+function multiply(a,b) {
+
+return `${a * b}`;
+
+}
+multiply(200,5);
+
+
+
+function divide(a,b) {
+
+  return `${a / b}`;
+}
+divide(100,2);
+
+
+function inc(n) {
+
+  return `${++n}`;
+
+}
+inc(4);
+
+
+function dec(n) {
+
+return `${--n}`;
+}
+dec(6);
+
+
+function makeInt(n) {
+
+return parseInt(n, 10);
+}
+
+
+
+
+
+function preserveDecimal(n) {
+ parseFloat(1.3567);
+ return `${n}`;
+}
