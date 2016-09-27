8a1cd91f46335f3fccd922fa734bb48dab58329c
Abrahym Sharfeldden <therealsharf@gmail.com>
2016-09-27 09:43:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function add(a,b)
+{
+  return a + b;
+}
+
+function subtract(a,b)
+{
+  return a - b;
+}
+
+function multiply(a,b)
+{
+  return a * b;
+}
+
+funtion divide(a,b)
+{
+  return a / b;
+}
