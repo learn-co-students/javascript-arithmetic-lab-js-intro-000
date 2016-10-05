759b9059b287bc4763da4323c4eb70800451d838
Katie Barclay <katiebarclay@icloud.com>
2016-10-05 00:09:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,41 @@
+function add(a,b){
+  var sum = a+b;
+  return sum;
+
+}
+
+function subtract(a,b){
+  var difference = a-b;
+  return difference;
+}
+
+function multiply(a,b){
+  var product = a*b;
+  return product;
+
+}
+
+function divide(a,b){
+  var quotient = a/b;
+  return quotient;
+}
+
+function inc(n){
+  var number = n+=1;
+  return number;
+}
+
+function dec(n){
+  var number = n-=1;
+  return number;
+}
+
+function makeInt(n){
+  var integer = parseInt(n,10);
+  return integer
+}
+
+function preserveDecimal(n){
+  var decimal = parseFloat(n);
+  return decimal;
+}
