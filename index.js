64c0d2dd96a6eaed4bd24bbddaa6f3bf2aa9e9a4
Caroline Hatwell <caroline.hatwell@gmail.com>
2016-10-12 01:48:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a,b) {
+  return a+b;
+}
+
+function subtract(a,b) {
+  return a-b;
+}
+
+function multiply(a,b) {
+  return a*b;
+}
+
+function divide(a,b){
+  return a/b;
+}
+
+function inc(n) {
+  var n;
+  return n += 1;
+}
+
+function dec(n) {
+  var n;
+  return n -= 1;
+}
+
+function makeInt(n) {
+  var n;
+  n= parseInt(n, 10);
+  return n;
+}
+
+function preserveDecimal(n) {
+
+  n= parseFloat(n);
+  return n;
+}
