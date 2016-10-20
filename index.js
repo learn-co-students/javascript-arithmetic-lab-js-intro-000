d0f3fc6d204ca6fea5d98c9f85ed2c975724423c
Tolulope Adediran <tolulopeadediran86@yahoo.com>
2016-10-20 02:00:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-function 4+2() {return '6'}
+function add(a,b) {return a+b}
 
-function 5-2() {return '3'}
+function subtract(a,b) {return a-b}
+
+function multiply(a,b) {return a*b}
+
+function divide(a,b) {return a/b}
+
+function inc(n) {return n += 1}
+
+function dec(n) {return n -= 1}
+
+function makeInt(n) {return parseInt(n,10)}
+
+function preserveDecimal(n) {return parseFloat(n)}
