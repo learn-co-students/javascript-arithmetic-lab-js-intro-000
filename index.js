df99ec6d2ea5dc941ea68171ee516e96c825a3eb
Francisco Galera <franciscojose@gmx.de>
2016-10-22 14:25:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(n1,n2){
+  return n1+n2
+}
+function subtract(n1,n2){
+  return n1-n2
+}
+function multiply(n1,n2){
+  return n1*n2
+}
+function divide(n1,n2){
+  return n1/n2
+}
+function inc(n){
+  n++
+  return n
+}
+function dec(n){
+  n--
+  return n
+}
+
+function makeInt(n){
+  n=parseInt(n,10)
+  return n
+}
+
+function preserveDecimal(n){
+  n=parseFloat(n)
+  return n
+}
