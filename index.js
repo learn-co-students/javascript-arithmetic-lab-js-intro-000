7e47e42abaad66f4aa8fb290f2ff9d4c8f73abb2
Michael Quiles <michaelquiles@gmail.com>
2016-10-24 23:35:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+  var plus = a + b
+  return plus
+}
+
+function subtract(a, b) {
+  var minus = a - b
+  return minus
+}
+
+function multiply(a, b) {
+  var times = a * b
+  return times
+}
+
+function divide(a, b) {
+  var into = a / b
+  return into
+}
+
+function inc(n) {
+  n++
+ return n
+}
+
+function dec(n) {
+  n--
+ return n
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
