a62df18f3271054812bbda60f24ffa1f3b80fea8
Ines Pucker <ines.pucker@gmx.de>
2016-10-25 18:48:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+function add (a,b){
+c=a+b;
+return c}
+
+
+function subtract (a,b){
+c=a-b;
+return c}
+
+
+function multiply (a,b){
+c=a*b;
+return c}
+
+
+function divide (a,b){
+c=a/b;
+return c}
+
+
+function inc(n) {
+n++;
+return n}
+
+function dec(n) {
+n--;
+return n}
+
+
+function makeInt (n){
+return (parseInt (n, 10))}
+
+function preserveDecimal(n){
+return (parseFloat(n))}
