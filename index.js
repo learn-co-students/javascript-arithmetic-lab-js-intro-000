d713a0507d62f0d4544fd178a0492c8150c48a83
Tami Abrams <graces.daughter@gmail.com>
2016-11-07 21:21:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function add(a, b){
+  return 700 + 53
+};
+
+function subtract(a, b) {
+  return 77 - 33
+};
+
+function multiply(a, b){
+  return 2 * 4
+};
+
+function divide(a, b) {
+  return 64 / 8
+};
