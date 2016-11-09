778efd3e6a230059f24bafd4cbc515f0f4c5ac49
Anna Brakowska <anna.brakowska91@gmail.com>
2016-11-09 03:02:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-var a, b
-
+// var a, b
 
 function add (a,b) {
-  return a+b;
+  var sum = a+b;
+  return sum;
+}
+
+
+function subtract (b,a) {
+  var minus = b-a;
+  return minus;
+}
+
+
+function multiply (a,b) {
+  var many = a*b;
+  return many;
 }
