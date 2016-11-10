fedd68ab4f169291ca1827455115481ac056f8b3
Tami Abrams <graces.daughter@gmail.com>
2016-11-10 20:25:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
 function add(a, b){
-  return 700 + 53
+  return a + b
 };
+
+add (700, 53)
 
 function subtract(a, b) {
-  return 77 - 33
+  return a - b
 };
+
+subtract (77, 33)
 
 function multiply(a, b){
-  return 2 * 4
+  return a * b
 };
 
+multiply (49, 35)
+
 function divide(a, b) {
-  return 64 / 8
+  return a / b
 };
+
+divide (55, 5)
+
+function inc(n) {
+  return ++n
+};
+
+inc (4)
+
+function dec(n) {
+  return --n
+};
+
+dec (5)
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+makeInt('2')
+
+function preserveDecimal(n) {
+  return parseFloat (n)
+}
+
+preserveDecimal ('howdy')
