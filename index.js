9dd84d6aa1bd3be40861ca0004b2ea728516f39c
Ignas nomatter <parazitaz@gmail.com>
2016-11-24 11:35:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+  var res= a + b;
+  return res;
+}
+function subtract(a,b) {
+  var res=a-b;
+  return res;
+}
+
+function multiply(a,b) {
+  var res=a*b;
+  return res;
+}
+
+function divide(a,b) {
+  var res=a/b;
+  return res;
+}
+function inc(a) {
+  a++;
+  return a;
+}
+
+function dec(a) {
+a--;
+  return a;
+}
+function makeInt(a) {
+  var res=parseInt(a,10);
+  return res;
+}
+function preserveDecimal(a) {
+  var res=parseFloat(a);
+  return res;
+}
