67c26579746f76e486d6c2314dcccd514037bdfb
idith almog <almog.idith@gmail.com>
2016-12-08 18:15:47 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -52,3 +52,7 @@ describe('preserveDecimal(n)', function() {
     expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
   })
 })
+function add(a,b) {return a+b}
+function subtract (a,b) {return a-b}
+function multiply (a,b) {return a*b)
+function divide (a,b) {return a/b}
