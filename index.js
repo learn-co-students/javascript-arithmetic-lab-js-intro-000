c8293bb3182b89d647adf897c3bd2f35f427c611
Lauren Aloy <laloy1089@gmail.com>
2016-12-08 01:02:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+function multiply(a, b) {
+  return a * b;
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function inc(n) {
+  ++n
+  return n
+}
+
+function dec(n) {
+  --n
+  return n
+}
+
+function makeInt(n) {
+ return parseInt(n, 10)
+ }
+
+function makeInt(n) {
+  return ('nonsense!',n)
+}
+
+function preserveDecimal(n) {
+  return parseFloat (n, 10)
+}
