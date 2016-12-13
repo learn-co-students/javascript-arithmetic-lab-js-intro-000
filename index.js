68295f7e92537c1eef9939a02da589ebb02547dc
Brandon Tyus <ausis@mail.com>
2016-12-13 17:36:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+var a = 1
+var b =10
+
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+function multiply(a, b) {
+  return a * b
+}
+function divide(a, b) {
+  return a / b
+}
+
+function inc(b) {
+  b++;
+  return b
+}
+
+function dec(b) {
+  b--;
+  return b
+}
+
+function makeInt(n) {
+  parseInt(n, 10)
+var x = parseInt(n, 10)
+return x
+}
+
+function preserveDecimal(z) {
+  return z
+}
+
+
+
