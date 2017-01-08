32f2ea0529ef8dfa65eca90ad0cc6a99f448be2e
Pavlin Penchev <pavlin.penchev@gmail.com>
2017-01-08 01:18:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,50 @@
+function add(a, b) {
+  return a + b
+}
+add(3, 4)
+
+function multiply(a, b) {
+  return a * b
+}
+multiply(3, 4)
+
+function subtract(a, b) {
+  return a - b
+}
+subtract(3, 4)
+
+function divide(a, b) {
+  return a / b
+}
+divide(3, 4)
+
+function inc(n) {
+  n++
+  return n
+}
+inc(10)
+
+
+function dec(n) {
+  n--
+  return n
+}
+dec(10)
+
+function makeInt(n) {
+   return parseInt(n, 10)
+}
+makeInt("2.34")
+
+function preserveDecimal(n) {
+   return parseFloat(n)
+}
+preserveDecimal("2.34")
+
+function preserveDecimal(n) {
+   return parseFloat(n)
+}
+preserveDecimal("Pavlin")
+
+
+
