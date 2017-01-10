f3d8d14b44464ce005998a6e7c687292c15a7d06
Jennifer Green <greenjenniferb@gmail.com>
2017-01-10 20:46:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a, b) {
+  return a + b;
+}
+add();
+
+function subtract(a, b) {
+  return a - b;
+}
+subtract();
+
+function multiply(a, b) {
+  return a * b;
+}
+multiply();
+
+function divide(a, b) {
+  return a / b;
+}
+divide();
+
+function inc(a) {
+  return a += 1;
+}
+inc();
+
+function dec(a) {
+  return a -= 1;
+}
+dec();
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+makeInt();
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
+preserveDecimal();
