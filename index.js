2bb397777ba7396036c5f7aee76e7aac10ef7139
KASSIM MULENDWE <kmulendwe@hotmail.com>
2017-01-28 22:13:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a,b) {
+  return (a + b)
+}
+
+function subtract(a,b) {
+  return (a - b)
+}
+
+function divide(a,b) {
+  return (a/b)
+}
+
+function multiply(a,b) {
+  return (a * b)
+}
+
+function inc(n) {
+  return ++n
+}
+
+function dec(n) {
+  return --n
+}
+
+function makeInt(n) {
+  var myInt = parseInt(n, 10);
+  return myInt
+}
+
+function preserveDecimal(n) {
+  var myFloat = parseFloat(n);
+  return myFloat
+}
