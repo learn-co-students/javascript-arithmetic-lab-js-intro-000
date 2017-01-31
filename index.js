6d727218efb5b9070132c65c8b32ad163404bada
Marcelo A Carrillo <marceloacarrillo@gmail.com>
2017-01-31 02:44:07 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function add () {
+
+}
+
+function subtract () {
+
+}
+
+function subtract () {
+
+}
+
+function multiply () {
+  
+}
+
+function multiply () {
+  
+}
+
+function divide () {
+  
+}
