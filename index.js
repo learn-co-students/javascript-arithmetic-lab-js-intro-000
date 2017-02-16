e5f672e3180ef89fa2fce0006bcb0a9c3b7c714a
Nunny Reyes <nunnyreyes@gmail.com>
2017-02-16 15:34:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+  return a + b
+} 
+
+function subtract (a, b) {
+  return a - b
+}
+
+function multiply (a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a/b
+}
+
+var number = 10
+function inc(n) {
+  return number ++ toEqual(404)
+}
+
+function dec(n) {
+  return number --
+}
+
+function makeInt(n) {
+  return parseInt (n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
+
