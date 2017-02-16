6c490fd59eff9bf8c71e9f92d2e1cff0ef1c99c0
Guy Bartlett <guylbartlett1@gmail.com>
2017-02-16 18:43:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,49 @@
+function add(a,b) {
+  return a + b
+}
+
+add(81,9)
+
+function subtract(a, b) {
+  return a - b
+}
+
+subtract(81,9)
+
+function multiply(a,b) {
+  return a * b
+}
+
+multiply(81,9)
+
+function divide(a,b) {
+  return a / b
+}
+
+divide(81,9)
+
+function inc(n) {
+  n++
+  return n
+}
+
+inc(81)
+
+function dec(n) {
+  n--
+  return n
+}
+
+dec(81)
+
+function makeInt(n) {
+  return parseInt(n,10)
+}
+
+makeInt("2")
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
+
+makeInt("2.313")
