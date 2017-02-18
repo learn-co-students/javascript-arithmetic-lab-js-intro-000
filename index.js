1dd8114e423e389c91886b6c5ba0f4b11fed3567
Sasha Bennet <sasha@babypterodactyl.com>
2017-02-18 03:39:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,48 @@
+function add(a,b) {
+  return (a + b)
+}
+
+add(5,3)
+
+function subtract(a,b) {
+  return (a - b)
+}
+
+subtract (10,3)
+
+function multiply(a,b) {
+  return (a * b)
+}
+
+multiply(5,3)
+
+
+function divide(a,b) {
+  return (a / b)
+}
+
+
+divide(15,5);
+
+function inc(n) {
+  return n += 1
+}
+
+inc()
+
+function dec(n) {
+return  n -= 1
+}
+
+dec()
+
+function makeInt(a) {
+  return parseInt(a,10);
+}
+
+makeInt('3.44');
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
+preserveDecimal('2.222');
