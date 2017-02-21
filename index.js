7cfe002c3a0207fc12fd7249d26b8b56c800851a
Ryan Stroh <ryan@rysen.com.au>
2017-02-21 03:57:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+function add(a, b) {
+  var add = a + b;
+  return add;
+}
+
+function subtract(a, b) {
+  var subtract = a - b;
+  return subtract;
+}
+
+function multiply(a, b) {
+  var multiply = a * b;
+  return multiply;
+}
+
+function divide(a, b) {
+  var divide = a / b;
+  return divide;
+}
+
+function inc(n) {
+  return n + 1;
+}
+
+function dec(n) {
+  return n - 1;
+}
+
+function makeInt(n) {
+  var n = parseInt(n, 10);
+  return n;
+}
+
+function preserveDecimal(n) {
+  var n = parseFloat(n);
+  return n;
+
+}
