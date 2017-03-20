4e8fb90dbd40fff929ffa6d2d916fd52f5109378
Melissa Lonsdale <mlonsdale@xogrp.com>
2017-03-20 02:25:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,42 @@
+var a = Math.random() * 1000
+var b = 480
+
+function add(a,b) {
+  return a += b 
+}
+
+function subtract(a,b) {
+  return a -= b
+}
+
+function multiply(a,b) {
+  return a *= b
+}
+
+function divide(a,b) {
+  return a /= b
+}
+
+var n = Math.random() * 1000
+
+function inc(n) {
+  return n += 1
+  
+}
+
+function dec(n) {
+  return n -= 1
+}
+
+var n = Math.random() * 1000
+
+function makeInt(n) {
+  return (parseInt(n, 10))
+  
+}
+
+var n = Math.random() * 1000
+
+function preserveDecimal(n) {
+  return (parseFloat(n))
+}
