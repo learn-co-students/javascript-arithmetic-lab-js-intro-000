0b321ee03d121583bbc6c6ea7c55af88d9335b55
Pasquale Rocchio <patrocc6@gmail.com>
2017-03-25 16:00:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(a) {
+  return a += 1
+}
+
+function dec(a) {
+  return a -= 1
+}
+
+function makeInt(num_string) {
+  return parseInt(num_string, 10)
+}
+
+function preserveDecimal(num_string) {
+  return parseFloat(num_string)
+}
