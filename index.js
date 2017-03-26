45eaeee74ab968fed6e912661c0c66e890daed8b
Nayeli C <nayelicruz12@gmail.com>
2017-03-26 21:53:23 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a,b) {
+  return a + b
+}
+
+function subtract(a,b) {
+  return a - b
+}
+
+function multiply(a,b) {
+  return a * b
+}
+
+function divide(a,b) {
+  return a / b
+}
+
+function inc(n) {
+  return n += 1
+}
+
+function dec(n) {
+  return n -= 1
+}
+
+function makeInt(n) {
+  return parseInt('a',10)
+  return parseInt('000',10)
+  return parseInt('sldkjflksjf',10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat('2.222')
+  return parseFloat('sldkjflksjf')
+
+
+}
