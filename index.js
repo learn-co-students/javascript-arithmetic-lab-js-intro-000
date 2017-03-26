91e7decd361480eeb51b3b044fc13dc26b9caa90
Jessica Pamanian <jexeones@gmail.com>
2017-03-26 17:28:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+return a + b
+}
+
+function subtract(a, b) {
+return a - b
+}
+
+function multiply(a, b) {
+return a * b
+}
+
+function divide(a, b) {
+return a / b
+}
+
+function inc(a) {
+  return a += 1
+}
+
+function dec(a) {
+  return a -= 1
+}
+
+function makeInt(string, radix) {
+  return parseInt(string, 10)
+}
+
+function preserveDecimal(string) {
+  return parseFloat(string)
+}
