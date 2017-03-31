81c5e7414fe9c4660184d0872dce416a4d70e0a3
Tom Breslin <tommybres@me.com>
2017-03-31 01:29:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,69 @@
+function add(a, b) {
+   return a + b;
+};
+
+function subtract(a, b) {
+  return a - b;
+};
+
+function multiply(a, b) {
+  return a * b;
+};
+
+function divide(a, b) {
+  return a / b;
+};
+
+var n = 0;
+function inc(n) {
+  n++;
+  return n;
+};
+
+function dec(n) {
+  n--;
+  return n;
+};
+
+function makeInt(n) {
+  if (parseInt('n', 10) === true) {
+  return n
+} else { return parseInt('n')
+};
+
+function add(a, b) {
+   return a + b;
+};
+
+function subtract(a, b) {
+  return a - b;
+};
+
+function multiply(a, b) {
+  return a * b;
+};
+
+function divide(a, b) {
+  return a / b;
+};
+
+var n = 0;
+function inc(n) {
+  n++;
+  return n;
+};
+
+function dec(n) {
+  n--;
+  return n;
+};
+
+function makeInt(n) {
+  if (parseInt('n', 10) === true) {
+  return n
+} else { return parseInt('n')
+};
+
+function preserveDecimal(n) {
+  return parseFloat('n')
+};
