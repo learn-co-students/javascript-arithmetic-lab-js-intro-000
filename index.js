48799508c536fb01d880447beb02ef857e1b0e6d
Dani Schuhman <dani8439@aol.com>
2017-05-05 16:44:06 +0000

    Done.

--- index.js
+++ index.js
@@ -15,15 +15,11 @@ function divide(a, b) {
 }
 
 function inc(n) {
-  return n += 1
+    return n +=1;
 }
 
 function dec(n) {
-    return n -= 1
-}
-
-function makeInt(n) {
-    return parseInt(n);
+    return n -=1;
 }
 
 function makeInt(n) {
