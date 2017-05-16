14c9135aecceff3c9d881465d6bd283f89518d36
Kirstyn Canull <kirstyn.nichole@gmail.com>
2017-05-16 00:34:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b){
+  var result = a + b;
+  return result
+}
+function subtract(a, b){
+  var result = a - b;
+  return result
+}
+function multiply(a, b){
+  var result = a * b;
+  return result
+}
+function divide(a, b){
+  var result = a / b;
+  return result
+}
+function inc(n) {
+  var result = n += 1;
+  return result
+}
+function dec(n) {
+  var result = n -= 1;
+  return result
+}
+function makeInt(n) {
+  var int = parseInt(n, 10);
+  return int
+}
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
