c068359f9a96c4ffd9451b783eaf71017709e4fd
Máté Rajnai <ramate.rajnai@gmail.com>
2017-05-28 09:50:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+var a = Math.floor(Math.random()*1000)
+var b = Math.floor(Math.random()*1000)
+
+function add(a,b) {return a+b}
+function subtract(a,b) {return a-b}
+function multiply(a,b) {return a*b}
+function divide(a,b) {return a/b}
+function inc(a) {return a += 1}
+function dec(a) {return a -= 1}
+
+function makeInt(n) {n = a; return parseInt(n)}
+function makeInt(n) {n = '0x2328'; return parseInt(n, 10)}
+function makeInt(n) {n = 'sldkjflksjf'; return parseInt('sldkjflksjf')}
+
+function preserveDecimal(n) {return parseFloat(n)}
+function preserveDecimal(n) {n = 'sldkjflksjf'; return parseFloat(n)}
