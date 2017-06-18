682afdb88b3e3f6d0135ac81bf5ba988221d7542
Aishah Amatullah <amatullah.aishah@gmail.com>
2017-06-18 00:33:47 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(n) {
+ n++;
+ return n;
+}
+
+function dec(n) {
+ n--;
+ return n;
+}
+
+function makeInt(n) {
+  var i = parseInt(n, 10);
+  return i;
+}
+
+function preserveDecimal(n) {
+  var f = parseFloat(n);
+  return f;
+}
