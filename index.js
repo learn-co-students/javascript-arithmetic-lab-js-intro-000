9b4d7e64e678fe98241d52591cfce22f4feb57d9
Victor  Mateo <vamateo85@gmail.com>
2017-07-20 22:09:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,47 @@
+
+function add (a,b){
+  return a + b ;
+}
+
+function subtract (a, b){
+  return a -b;
+}
+
+function multiply (a, b){
+  return a * b;
+}
+
+ function divide (a, b){
+   return a / b;
+ }
+
+function inc (n){
+  return n+=1 ; // increment N value
+}
+
+
+function dec (n){
+  return n -=1 ; // decrement N value
+}
+
+
+function makeInt(a){
+  return a.toString();
+}
+
+function makeInt(n){
+ return parseInt(n, 10);
+}
+
+
+function makeInt(n){
+   return parseInt('sldkjflksjf', 10);
+}
+
+function preserveDecimal(n){
+  return parseFloat('2.222');
+}
+
+function preserveDecimal(n){
+   return parseFloat(n);
+}
