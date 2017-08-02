db4da2f7faea4fead3d7a80052c7cfdb6ed367a0
L North <lnordell@gmail.com>
2017-08-02 21:38:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-function add(a,b){
-  return (a+b);
+function add(a, b){
+  return a+b;
 }
 
-function subtract(a,b){
-  return (a-b);
+function subtract(a, b){
+  return a-b;
 }
 
 function multiply(a,b){
-  return (a*b);
+  return a*b;
 }
 
 function divide(a,b){
-  return (a/b);
+  return a/b
 }
 
 function inc(n){
-  return (n += 1);
+  return n+=1;
 }
 
 function dec(n){
-  return(n -= 1);
+  return n-=1;
 }
 
 function makeInt(n){
@@ -27,5 +27,6 @@ function makeInt(n){
 }
 
 function preserveDecimal(n){
-  return parseFloat(n);
+  parseFloat(n);
+  return n;
 }
