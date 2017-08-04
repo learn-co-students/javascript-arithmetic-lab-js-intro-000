c14a9f6b479985cb8ce7872a9b21c98636f38243
Juan Montoya <jcmontoya@gmail.com>
2017-08-04 01:58:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+  var add = (a + b)
+  return add
+}
+
+function subtract(a, b) {
+  var subtract = (a - b)
+  return subtract
+}
+
+function multiply(a, b) {
+  var number = (a * b)
+  return number
+}
+
+function divide(a, b) {
+  var number = (a / b)
+  return number
+}
+
+function inc(a) {
+  var num = (a + 1)
+  return num
+}
+
+function dec(a) {
+  var num = (a - 1)
+  return num
+}
+
+function makeInt(n) {
+  return (parseInt(n, 10))
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
