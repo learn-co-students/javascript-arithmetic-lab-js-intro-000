24b14296a78e939177c9517108aa3e6ec982586a
Jonathan Tross <jontross@hotmail.com>
2017-08-07 20:18:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function inc(n) {
+  return n += 1;
+}
+
+function dec(n) {
+  return n -= 1;
+}
+
+function makeInt(n) {
+  if (n === NaN) {
+    return NaN;
+  }
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+  if (n === NaN) {
+    return NaN;
+  }
+  return parseFloat(n);
+}
