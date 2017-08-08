7f57ab9969e918d940b6a0f3cea92248186e5c03
Jeff Donnelly <jdonnelly21@gmail.com>
2017-08-08 21:05:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,43 @@
+var a = 1
+var b = 80
+
+function add(a, b) {
+  return a + b
+}
+
+var a = 60
+var b = 40
+
+function subtract(a, b){
+ return a - b
+}
+
+var a = 2
+var b = 3.4
+
+function multiply(a, b){
+ return a * b
+}
+
+var a = 5.0
+var b = 2.5
+
+function divide(a, b){
+  return a / b
+}
+
+function inc(n) {
+  return (n + 1)
+}
+
+function dec(n){
+  return (n - 1)
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
