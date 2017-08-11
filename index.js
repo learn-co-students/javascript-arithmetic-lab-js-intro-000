448f280f2b80d6d204bd457047d758311978ed6a
Steven Oladipo <stevenboladipo@gmail.com>
2017-08-11 00:50:02 +0000

    Done.

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ function subtract(a, b){
   return a - b;
 }
 
-multiply(a ,b){
+function multiply(a, b){
   return a * b;
+}
+
+function divide(a, b){
+  return a / b;
+}
+
+function inc(n) {
+  return ++n;
+}
+
+function dec(n){
+  return --n;
 }
