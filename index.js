e76ccfc6353a878d7bde71347d5daf3eb6ca5ba9
Aaron R. Trevino <aaronrtrevino12@gmail.com>
2017-10-05 16:38:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var a, b
 
-a = Math.floor(Math.random() * 1000)
+a = Math.floor(Math.random() * 1000)  // generate a random number, round it down (Math.floor) to a whole number, and max = 999
 b = Math.floor(Math.random() * 1000)
 
 function add(a, b) {
