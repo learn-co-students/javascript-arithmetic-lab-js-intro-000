2423245444bd09ae00038b592ee5e1ae5a527a21
EPIC448 <samrey2018@gmail.com>
2017-10-06 16:26:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+//Notes: Im just using the words used in the describition.. i.e add,subtracr
+function add(n1, n2) {
+  return n1 + n2
+}
+
+function multiply(n1, n2) {
+  return n1 * n2
+}
+
+function subtract(n1, n2) {
+  return n1 - n2
+}
+
+function divide(n1, n2) {
+  return n1 / n2
+}
+
+function inc(n) {
+  return ++n
+
+}
+
+function dec(n){
+  return --n
+}
+
+function makeInt(n) {
+  return n
+}
+
+function makeInt('n', 10){
+  return n
+}
