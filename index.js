3e8ca00e7d836bb172f0ad63a5548a54b3d6cb09
April Rivera <aprilcr87@gmail.com>
2017-10-22 14:06:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a,b){
+  return `${a + b}`
+}
+add(3,4);
+function subtract(a,b){
+  return `${a - b}`
+}
+subtract(10, 5);
+function multiply(a,b){
+  return `${a * b}`
+}
+multiply(3, 4);
+function divide(a,b){
+  return `${a / b}`
+}
+divide(10, 2);
+function inc(n){
+  return n = n+1
+}
+inc(2);
+function dec(n){
+  return n = n-1
+}
+dec(2);
+function makeInt(n){
+return parseInt(n, 10)
+}
+
+makeInt('3');
+function preserveDecimal(n){
+  return parseFloat(n, 10)
+}
+preserveDecimal('8.00');
