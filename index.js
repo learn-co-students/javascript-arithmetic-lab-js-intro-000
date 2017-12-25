b0a3b5635919cb8c5b544d396fcff871a53c2757
abbasqureshi7 <abbasqureshi7@gmail.com>
2017-12-25 04:07:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,63 @@
+var a, b
+
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+
+function add(a, b) {
+  return a + b;
+}
+console.log(add);
+
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
+
+function subtract(a, b) {
+  return a - b;
+}
+console.log(subtract);
+
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
+
+function multiply(a, b) {
+  return a * b;
+}
+console.log(multiply);
+
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
+
+function divide(a, b) {
+  return a / b;
+}
+console.log(divide);
+
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
+
+function inc(n) {
+  return (n+= 1);
+}
+console.log(inc);
+
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
+
+function dec(n) {
+  return (n-= 1);
+}
+console.log(dec);
+
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
+
+function makeInt(n) {
+  return parseInt(n, 10);
+};
+
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+};
