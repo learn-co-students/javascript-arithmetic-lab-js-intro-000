0681b90c8fe01b4db9ad7e2dcc06afbc4d20a469
Richa Gode <richa.gode@gmail.com>
2017-12-30 03:52:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(num1, num2) {
+  return num1 + num2
+}
+
+function subtract(num1, num2) {
+  return num1 - num2
+}
+
+function multiply(num1, num2) {
+  return num1 * num2
+}
+
+function divide(num1, num2) {
+  return num1/num2
+}
+
+function inc(num) {
+  var value = num + 1
+  return value
+}
+
+function dec(num) {
+  var value = num -1
+  return value
+}
+
+function makeInt(n) {
+  return parseInt(n,10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n, 10)
+}
