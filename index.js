b39d536297ca96776d5324f3163a9832cf2444ad
dariodlearnco <dario.dorella@gmail.com>
2018-01-05 03:09:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function add(a, b) { return a + b }
+
+function subtract(a, b) { return a - b }
+
+function multiply(a, b) { return a * b }
+
+function divide(a, b) { return a / b }
+
+function inc(n) { return n += 1 }
+
+function dec(n) { return n -= 1 }
+
+function makeInt(input) { return parseInt(input, 10) }
+
+function preserveDecimal(input) { return parseFloat(input) }
