da6ebf7030ba353f8058b0e2233c6445cfb85e6a
mark melesse <mark.melesse@gmail.com>
2018-01-13 20:13:32 +0000

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,17 @@ function divide(a,b) {
   return a / b;
 }
 function inc(n) {
-  return ++63 ;
+  return n += 1
+}
+
+function dec(n) {
+  return n -= 1
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
 }
