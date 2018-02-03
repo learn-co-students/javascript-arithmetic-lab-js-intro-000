4f664cdec142f8b2bcf1253872e7637b3bb857d8
Lucy <marerescue@gmx.com>
2018-02-03 05:39:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,65 @@
+var number;
+function add(a, b){ //add
+  number = a + b;
+  return number;
+}
+function subtract(a, b){ //subtract
+  number = a - b;
+  return number;
+}
+function multiply(a, b){ //multiply
+  number = a * b;
+  return number;
+}
+function divide(a, b){ //divide
+  return number;
+}
+function inc(a){ //Incre
+  number = a + 1;
+  return number;
+}
+function dec(a){ //Dec
+  number = a - 1;
+  return number;
+}
+function makeInt(a){ //Parse
+  number = parseInt(a, 10);
+  return number;
+}
+function preserveDecimal(a){ //Parse
+  number = parseFloat(a, 10);
+  return number;
+}
+var number;
+function add(a, b){ //add a and b
+  number = a + b;
+  return number;
+}
+function subtract(a, b){ //subtract
+  number = a - b;
+  return number;
+}
+function multiply(a, b){ //multiply
+  number = a * b;
+  return number;
+}
+function divide(a, b){ //divide
+  number = a / b;
+  return number;
+}
+function inc(a){ //Increment
+  number = a + 1;
+  return number;
+}
+function dec(a){ //Decrement
+  number = a - 1;
+  return number;
+}
+function makeInt(a){ //Parse a
+  number = parseInt(a, 10);
+  return number;
+}
+function preserveDecimal(a){ //Parse a
+  number = parseFloat(a, 10);
+  return number;
+}
