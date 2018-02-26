0c99c2c406386e13d6ac35785de78eaf74d5f5e3
Eliot Garcia <eag0828@gmail.com>
2018-02-26 23:16:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+
+function add(a, b) {
+  return a += b;
+}
+add()
+
+function subtract(a, b) {
+  return a -= b;
+}
+subtract()
+
+function multiply(a, b) {
+  return a *= b;
+}
+multiply()
+
+function divide(a, b) {
+  return a /= b;
+}
+divide()
+
+function inc(n) {
+  return ++n;
+}
+inc()
+
+function dec(n) {
+  return --n;
+}
+dec()
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
