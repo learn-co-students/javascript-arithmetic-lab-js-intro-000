6983e8df054d9085572340d6823f7b9ab94741da
tarheelnc83@outlook.com <tarheelnc83@outlook.com>
2018-03-06 17:22:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+var number
+
+function add(a,b){
+  return number=a+b
+}
+
+function subtract(a,b){
+  return number=a-b
+}
+
+function multiply(a,b){
+  return number=a*b
+}
+
+function divide(a,b){
+  return number=a/b
+}
+
+function inc(n){
+  return number=n+1
+}
+
+function dec(n){
+  return number=n-1
+}
+
+function makeInt(n){
+  return number=parseInt(n,10)
+}
+
+function preserveDecimal(n){
+  return number=parseFloat(n)
+}
+
+add(20,60)
