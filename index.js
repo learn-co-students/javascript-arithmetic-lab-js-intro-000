c1185569eb740e130a9a89b8ed892717c7913dd3
marcantoineandreoli@gmail.com Andreoli <marcantoineandreoli@gmail.com>
2018-03-13 07:20:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function add(a,b) {
+  return a + b
+}
+
+function subtract(a,b) {
+  return a - b
+}
+
+function multiply(a,b) {
+  return a * b
+}
+
+function divide(a,b) {
+  return a / b
+}
+
+function parseInt(a,b) {
+
+}
+
+var n = 10
+
+function inc(n) {
+  n+=1
+}
+
+function dec(n) {
+n-=1
+}
