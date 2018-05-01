8b50f5a921692790a81fcf05140808744d99bd3f
Rowan Lucey <rslucey@gmail.com>
2018-05-01 20:37:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+  return (a + b)
+}
+
+function subtract(a, b) {
+  return (a - b)
+}
+
+function multiply(a, b) {
+  return (a * b)
+}
+
+function divide(a, b) {
+  return (a / b)
+}
+
+function makeInt(n) {
+  var a = parseInt(n, 10);
+  return a;
+}
+
+function preserveDecimal(n) {
+  var a = parseFloat(n);
+  return a;
+}
+
+function inc(n) {
+  return (n + 1)
+}
+
+function dec(n) {
+  return (n - 1)
+}
