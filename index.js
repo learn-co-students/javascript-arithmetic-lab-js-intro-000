b86cbd785e1c18f05fe81c2ae4ff042de5a544f4
Cheryl Kelson <cherylkelson@icloud.com>
2018-06-03 17:03:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+5 + 7;
+5 -2;
+3 * 9;
+16 /4;
+var number = 165;
+number++;
+number;
+number--;
+number;
+parseInt('5', 12);
+parseInt('14', 10);
+parseInt('gobbledygook!', 10);
+parseFloat('14.7899');
+
+
+
