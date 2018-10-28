ad7667524a274f49d5015f23d7f55b54f8422d6d
Raiyan Rahman <raiyan.rahman4@gmail.com>
2018-10-28 02:10:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+//Function returns the sum of two numbers.
+function add(a, b) {
+  return a + b
+}
+
+//Function returns the difference of two numbers.
+function subtract(a, b) {
+  return a - b
+}
+
+//Function returns the product of two numbers.
+function multiply(a, b) {
+  return a * b
+}
+
+//Function returns the quotient of two numbers.
+function divide(a, b) {
+  return a / b
+}
+
+//Function takes user input and increments it by 1.
+function inc(n) {
+  return n += 1;
+}
+
+//Function takes user input and decrements it by 1.
+function dec(n) {
+  return n -= 1;
+}
+
+//Function takes user input as string and returns an integer value.
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+//Function takes user input as string and returns a floating point value.
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
+
