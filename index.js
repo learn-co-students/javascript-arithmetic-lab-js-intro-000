7768e4fb23a845539d487ff41f7e02272de98e29
lanasgoddard <lanasgoddard@gmail.com>
2018-11-08 13:39:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+var number;
+function add(a,b) {
+  number = a+b
+  return number
+}
+
+function subtract(a,b) {
+  number = a-b
+  return number
+}
+
+function multiply(a,b) {
+  return number = a*b
+}
+
+function divide(a,b) {
+ number = a/b
+ return number
+}
+
+function inc(n) {
+  number = n+1
+  return number
+}
+
+function dec(n) {
+  number = n-1
+  return number
+}
+function makeInt(n){
+  number =  parseInt(n, 10)
+  return number
+}
+
+function preserveDecimal(n){
+  number = parseFloat(n)
+  return number
+}
