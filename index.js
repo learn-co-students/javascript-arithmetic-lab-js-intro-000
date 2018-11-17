6f079857dd7e146a25a998c8c47907bf9d8d6e8d
Axel Daniela Campero Vega <axelcampero@hotmail.com>
2018-11-17 18:54:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a,b){
+  var a
+  var b
+  return a+b
+}
+function subtract(a,b){
+  var a
+  var b
+  return a-b
+}
+function multiply(a,b){
+  var a,b;
+  return a*b
+}
+function divide(a,b){
+
+  return a/b
+}
+function inc(n){
+  var n
+  return n+=1
+}
+function dec(n){
+  return n-=1
+}
+function makeInt(n){
+  var n
+  return parseInt(n,10)
+}
+function preserveDecimal(n){
+  return parseFloat(n)
+}
