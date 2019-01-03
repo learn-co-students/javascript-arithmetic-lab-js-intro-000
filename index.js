80f48d310011c18bd7bb945b9a6d1f53628d6bcd
cerebral-map-4184 <patriciagbarrett@gmail.com>
2019-01-03 17:48:49 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a,b) {
+  return a+b
+}
+
+function subtract(a,b) {
+  return a-b
+}
+
+function multiply(a,b) {
+  return a * b
+}
+
+function divide(a,b) {
+  return a/b
+}
+
+function inc(three) {
+  return three +=1
+}
+
+function inc(three) {
+  return three +=1
+}
+
+function dec(four) {
+  return four -= 1
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
