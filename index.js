c9e395ba07106d013376d1c5a9e26edc6b30381e
Sridevi Patchipulusu <sridevips@hotmail.com>
2019-01-22 03:39:49 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function add(8,10){
+  return "result"
+}
+function subtract(8,10) {
+  return "result"
+}
+  function multiply(8,10){
+
+  return "result"
+}
+function divide(8,10) {
+  return "result"
+}
