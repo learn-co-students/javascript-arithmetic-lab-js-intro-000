39f6def62d95a0d51579b91c330fcff2702fd2c2
Tanuka Das <das.tanuka@yahoo.com>
2019-03-24 15:06:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,46 @@
+
+function add(a, b){
+  var result = b + a;
+  return result;
+}
+add();
+
+function subtract(a, b){
+  var result = a - b;
+  return result;
+}
+subtract();
+
+function multiply(a, b){
+  var result = a * b;
+  return result;
+}
+multiply();
+
+function divide(a, b){
+  var result = a/b;
+  return result;
+}
+divide();
+
+
+function inc(n){
+
+  var result = ++n;
+  return result;
+}
+
+function dec(n){
+  var result = --n;
+  return result;
+}
+
+
+function makeInt(n){
+  return parseInt(n, 10);
+  //return makeInt();
+}
+
+function preserveDecimal(n){
+  return parseFloat(n);
+}
