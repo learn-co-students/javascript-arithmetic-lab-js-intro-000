b8d624620d34a320d7d8d3a62b1d5cb7ff766b4f
glorious-tier-7619 <thomasepowell@icloud.com>
2019-04-16 14:45:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+
+  return a+b
+}
+
+function subtract(a, b) {
+  return a-b
+}
+
+function multiply(a, b) {
+  return a*b
+}
+
+function divide(a, b) {
+  return a/b
+}
+
+function inc(n) {
+  var mynum = n
+  mynum++
+  return mynum
+}
+
+function dec(n) {
+  var mynum = n
+  mynum--
+  return mynum
+}
+function makeInt(n) {
+  var mynum = (parseInt(n, 10))
+  return mynum
+}
+
+function preserveDecimal(n) {
+  var mynum = (parseFloat(n))
+  return mynum
+}
