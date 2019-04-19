736136f11c9183be6158469653cf412e1b2869c5
Kishore Chandrasekharan <kishore28m@gmail.com>
2019-04-19 17:04:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+
+function add(a,b){
+  return a + b;
+}
+
+function subtract(a,b){
+  return a - b;
+}
+
+function multiply(a,b){
+  return a * b;
+}
+
+function divide(a,b){
+  return a/b;
+}
+
+function inc(a){
+    a++;
+  return a;
+}
+
+function dec(a){
+  a--;
+  return a ;
+}
+
+function  makeInt(a){
+  var z = parseInt(a,10);
+  return z;
+}
+
+function preserveDecimal(a) {
+  var d = parseFloat(a);
+  return d;
+}
