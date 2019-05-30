c3cd95185475989b7cc5d64cefa58e0b388b795e
Tara Wright <wrightt2015@gmail.com>
2019-05-30 00:36:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,52 @@
+var a = 98;
+    b = 288;
+
+function add(a, b){
+  return a + b;
+}
+
+
+var a = 60;
+    b = 121;
+function subtract(a,b){
+  return a-b;
+}
+
+
+var a = 11;
+    b = 14;
+function multiply(a,b){
+  return a * b;
+}
+
+
+var a = 15;
+    b = 5;
+function divide(a,b){
+  return a / b;
+}
+
+var n = 790;
+function inc(n){
+  n++;
+  var x = n;
+  return x;
+}
+
+
+var n = 91;
+function dec(n){
+  n--;
+  var z = n;
+  return z;
+}
+
+var n = 4.323;
+function makeInt(n){
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n){
+  n = 2.222;
+  return parseFloat(n);
+}
