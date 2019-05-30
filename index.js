b4502a34994274b6fd15fec7474172bafde105a8
Tara Wright <wrightt2015@gmail.com>
2019-05-30 01:45:34 +0000

    Done.

--- index.js
+++ index.js
@@ -28,17 +28,13 @@ function divide(a,b){
 
 var n = 790;
 function inc(n){
-  n++;
-  var x = n;
-  return x;
+  n+= 1;
 }
 
 
 var n = 91;
 function dec(n){
-  n--;
-  var z = n;
-  return z;
+  n-= 1;
 }
 
 var n = 4.323;
@@ -46,12 +42,7 @@ function makeInt(n){
   return parseInt(n, 10);
 }
 
-function preserveDecimal(n){
-  n = 2.222;
-  return parseFloat(n);
-}
 
-///var n = 2.222
 function preserveDecimal(n){
   return parseFloat(n);
 }
