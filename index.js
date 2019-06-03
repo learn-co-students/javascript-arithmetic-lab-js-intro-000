78dc8c92727b2bb0bbbea13fe2f52655d0c01032
Gabriella Ilena <ilenagabriella@gmail.com>
2019-06-03 13:58:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,41 @@
+//Arithmetic operations in JavaScript
+
+//Variable declarations
+var a = 10
+var b = 5
+var n = 100
+
+//Functions
+function add(a, b) {
+  return (a + b)
+}
+
+function subtract(a, b) {
+  return (a - b)
+}
+
+function multiply(a, b) {
+  return (a * b)
+}
+
+function divide(a, b) {
+  return (a / b)
+}
+
+function inc(n) {
+  n++
+  return (n)
+}
+
+function dec(n) {
+  n--
+  return (n)
+}
+
+function makeInt(n) {
+  return (parseInt(n, 10))
+}
+
+function preserveDecimal(n) {
+  return (parseFloat(n))
+}
