b06271d88408d1661f99a972b591cf17e9d479bc
Joshua Choi <jsc.r839@gmail.com>
2019-06-06 23:43:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a, b){
+  return a + b;
+}
+
+function subtract(a, b){
+  return a - b;
+}
+
+function multiply(a, b){
+  return a * b;
+}
+
+function divide(a, b){
+  return a / b;
+}
+
+function inc(n){
+  var answer = n++
+  return answer;
+}
+
+function dec(n){
+  var answer = n--
+  return answer;
+}
+
+function makeInt(n){
+  var answer = parseInt(n, 10)
+  return answer;
+}
+
+function preserveDecimal(n){
+  var answer = parseFloat(n)
+  return answer;
+}
