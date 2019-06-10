f80d636500c54ab47455378e96d809aadf28d961
Justin Nowicki <justinnowicki@gmail.com>
2019-06-10 13:33:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a, b){
+  a += b
+  return a;
+}
+
+function subtract(a, b) {
+  a -= b
+  return a;
+}
+
+function multiply(a, b) {
+  a *= b
+  return a;
+}
+
+function divide(a, b) {
+  a /= b
+  return a;
+}
+
+function inc(n) {
+  n++
+  return n;
+}
+
+function dec(n) {
+  n--
+  return n;
+}
+
+function makeInt(n) {
+  parseInt('n', 10);
+  return n;
+}
+
+function preserveDecimal(n) {
+  parseFloat('n');
+  return n;
+}
