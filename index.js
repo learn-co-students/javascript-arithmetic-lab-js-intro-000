594a86928f335fbba86df2bcd148f7398150c8d6
Elfi Yang <elfiyang16@gmail.com>
2019-07-22 19:12:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function add(a,b){
+  return a+b
+}
+
+1+3
+
+1-3
+
+1*3
+
+1/3
+
+1++
+
+1--
+
+parseInt(`2`, 10)
+
+parseInt(`9`, 10)
+
+parseInt(`wtf`)
+
+parseFloat(`2.32432543`)
+
+parseFloat(`aserware`)
