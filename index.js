1849e0b95e60e0ab4492fb8c4e5c225e2f1ca0b5
Ruoxin Lin <ruoxinlin.ltg@gmail.com>
2019-07-24 02:47:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+var a = 8
+    b = 4;
+
+function add(a,b) {
+ return a + b
+}
+function subtract(a,b) {
+  return a-b
+}
+function multiply(a,b) {
+  return a*b
+}
+function divide(a,b) {
+  return a/b
+}
+function inc(n) {
+  return n+1
+}
+function dec(n) {
+  return n-1
+}
+function makeInt(n) {
+  return n
+}
+function makeInt(n) {
+  return parseInt(n,10)
+}
+function preserveDecimal(n) {
+  return parseFloat(n,6.6666)
+}
