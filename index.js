217066c0c1cdc84adeb03acf151f3796d908f959
voluble-block-7446 <iagogomes80@gmail.com>
2019-07-26 20:08:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a,b){
+  var r= a+b;
+  return r;
+}
+function subtract(a,b){
+  var r=a-b;
+  return r;
+}
+function multiply(a,b){
+  var r=a*b;
+  return r;
+}
+function divide(a,b){
+  var r=a/b;
+  return r;
+}
+function inc(n){
+  var r= n+1;
+  return r;
+}
+function dec(n){
+  var r= n-1;
+  return r;
+}
+function makeInt(n){
+  return parseInt(n,10);
+}
+function preserveDecimal(n){
+  return parseFloat(n);
+}
