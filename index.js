b6cb7085bb332f922f20adebd34243750888b5cd
diplomatic-database-6969 <shopokov.tilek@mail.ru>
2019-07-30 07:46:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(a, b) {
+  return a + b;
+}
+add(1,2);
+function subtract(a, b) {
+  return a - b;
+}
+subtract(3,2);
+function multiply(a, b) {
+  return a * b;
+}
+multiply(3, 2);
+function divide(a, b) {
+  return a / b;
+}
+divide(3,2);
+function inc(n) {
+  n++;
+  return n;
+}
+inc(6);
+divide(3,2);
+function dec(n) {
+  n--;
+  return n;
+}
+dec(6);
+function makeInt(n) {
+  parseInt(n);
+  return n;
+}
+makeInt("6");
+
+function makeInt(n) {
+  return parseInt(n, 10);
+  }
+makeInt("2");
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
