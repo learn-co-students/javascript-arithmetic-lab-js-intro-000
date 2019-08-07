202dd0b2db2c89223f81ff4f1883e7fbbe6009dc
Nick Spera <nickaspera@gmail.com>
2019-08-07 20:01:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a,b){
+  return a+b
+}
+
+function subtract(a,b) {
+  return a-b
+}
+
+function multiply(a,b) {
+  return a*b
+}
+
+function divide(a,b) {
+  return a/b
+}
+
+function inc(n) {
+  return n + 1
+}
+
+function dec(n) {
+  return n - 1
+}
+/*
+function makeInt(n, base) {
+  var parsed = parseInt(n, base)
+  return parsed * 10;
+}
+ function makeInt(n, base) {
+   var base = 10;
+ }
+*/
+
+function preserveDecimal(n) {
+  return n
+}
