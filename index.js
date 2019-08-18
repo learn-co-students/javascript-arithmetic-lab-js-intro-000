595f860b9afccf88c99217e85ec56267b666e85e
Stephen Robert hall <steve.r.hall@outlook.com>
2019-08-18 11:20:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,41 @@
+function add(a, b) {
+  var sum = a + b
+  return sum
+}
+
+function subtract(a, b) {
+  var reslt = (a - b)
+  return reslt
+}
+
+function multiply(a, b) {
+  var sum = (a * b)
+  return sum
+}
+
+function divide(a, b) {
+  var dividend = (a / b)
+  return dividend
+}
+
+function inc(n) {
+var rslt = n
+rslt++
+  return rslt
+}
+
+function dec(n) {
+  var rslt = (n)
+  rslt--
+    return rslt
+}
+
+function makeInt(n) {
+  var retval = parseInt(n, 10)
+  return retval
+}
+
+function preserveDecimal(n) {
+  var retval = parseFloat(n)
+  return retval
+}
