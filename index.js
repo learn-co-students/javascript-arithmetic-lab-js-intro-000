7c4753b83823e38e76d76083af49ab2a746fc0ca
Abdulbaasit <baasisek01@gmail.com>
2019-08-25 21:20:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+var a, b;
+function add(a, b){
+  var addition = a + b
+  return addition
+}
+function subtract(a, b){
+  var subtraction = a - b
+  return subtraction
+}
+function multiply(a, b){
+  var multiplication = a * b
+  return multiplication
+}
+function divide(a, b){
+  var division = a / b
+  return division
+}
