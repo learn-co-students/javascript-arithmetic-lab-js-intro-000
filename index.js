9cbb8b38f207eb744d1f4f22ec9b62024fdcd0aa
Josia Rodriguez <josiarodriguez@gmail.com>
2019-09-08 14:00:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+let add = function(a, b) {
+  return a + b;
+}
+
+let subtract = function(a,b) {
+  return a - b;
+}
+
+let multiply = function(a,b) {
+  return a * b;
+}
+
+let divide = function(a, b) {
+  return a/b;
+}
+
+let inc = function(n) {
+  return n += 1;
+}
+
+let dec = function(n) {
+  return n -= 1;
+}
+
+let makeInt = function(a){
+  return parseInt(a, 10);
+}
+
+let preserveDecimal = function(a) {
+  return parseFloat(a);
+}
