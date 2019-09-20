cfa2070053638fef9d0ea3a6ee1b8dd7eac8a106
Howard Shiau <howardshiau@yahoo.com>
2019-09-20 21:44:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(f1,f2) {
+  return f1+f2
+}
+
+function subtract(f1,f2) {
+  return f1-f2
+}
+
+function multiply(f1,f2) {
+  return f1*f2
+}
+
+function divide(f1,f2) {
+  return f1/f2
+}
+
+function inc(f1) {
+  return f1 += 1
+}
+
+function dec(f1) {
+  return f1 -= 1
+}
+
+function makeInt(f1) {
+  return parseInt(f1,10)
+}
+
+function preserveDecimal(f1) {
+  return parseFloat(f1)
+}
