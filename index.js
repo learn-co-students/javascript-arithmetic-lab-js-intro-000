6fab3bd393e7dc508fdf89817b77232727b5b102
itai rosenberg <djitai69@gmail.com>
2019-10-14 09:02:46 +0000

    Done.

--- index.js
+++ index.js
@@ -13,18 +13,18 @@ function divide(a, b){
   return a / b;
 }
 
-function inc(){
-  return n+=
+function inc(n){
+  return n += 1
 }
 
-function dec(x){
-console.log(x--)
+function dec(n) {
+  return n -= 1
 }
-
-
-
-
 
 function makeInt(n){
   return parseInt(n,10)
+}
+
+function preserveDecimal(n){
+  return parseFloat(n)
 }
