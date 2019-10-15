1022edb7edd3dd14922217632ede32ac31d700f8
Carly Lord <carly.lord@gmail.com>
2019-10-15 03:15:55 +0000

    Done.

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ function divide(a,b){
   return a/b
 }
 function inc(n){
-  return ++n
+  return n+=1
 }
 function dec(n){
-  return --n
+  return n+=-1
 }
 function makeInt(n){
   return parseInt(n,10)
