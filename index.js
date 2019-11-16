3f12d0598ba76c7b5af6416353bafec15e8f4ed8
Maxim Clefos <mk.clefos@gmail.com>
2019-11-16 16:14:37 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,41 @@
+var a=50
+var b=2000
+function add(a,b){
+  return (a+b)
+}
+
+var a=100
+var b=36
+function subtract(a,b){
+  return (a-b)
+}
+
+var a=6
+var b=2
+function multiply(a,b){
+  return (a*b)
+}
+
+var a=8
+var b=4
+function divide(a,b){
+  return (a/b)
+}
+
+var n=5
+function inc(n){
+  return (n+1)
+}
+
+var n=5
+function dec(n){
+  return (n-1)
+}
+
+function makeInt(n){
+  return parseInt (n, 10)
+}
+
+ function preserveDecimal (n){
+   return parseFloat (n)
+ }
