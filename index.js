3c9dbf1b48f64416e1dfccfd79ac538600abdff7
John Schimansky <schimmy@gmail.com>
2019-12-04 21:38:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(a, b) {
+  return a + b
+}
+function subtract(a, b) {
+  return a - b
+}
+function multiply(a, b) {
+  return a * b
+}
+function divide(a, b) {
+  return a / b
+}
+var n = 643
+function inc(n) {
+  return n += 1
+}
+function dec(n) {
+  return n -= 1
+}
+function makeInt(n) {
+  return parseInt('0', 10)
+}
+function makeInt(n) {
+  return parseInt("lwjv", 10)
+}
+function preserveDecimal(n) {
+  return parseFloat('2.222')
+}
