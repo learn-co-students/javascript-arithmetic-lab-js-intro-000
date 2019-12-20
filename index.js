9ee0fb763794c39226eb9f2e98abc8372bf3420b
Jim Rountree <jimvrountree@gmail.com>
2019-12-20 00:41:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+return a + b
+}
+function subtract(a,b){
+  return a - b
+}
+function multiply(a,b) {
+  return a * b
+}
+function divide(a, b) {
+  return a/b
+}
+function inc(a) {
+  return a + 1
+}
+function dec(a) {
+  return a - 1
+}
+function makeInt(n) {
+  return parseInt(n)
+  return parseInt(0)
+  return parseInt(true)
+}
