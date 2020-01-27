3b697eaf9378238b9890b642016e71d6f66a8344
James Stamps Jr <jstampshere@gmail.com>
2020-01-27 19:14:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+var num = (1, 80)
+function add(num1, num2) {
+return(num1 + num2)
+}
+
+var num = (60, 40)
+function subtract(num1, num2) {
+return(num1 - num2)
+}
+
+var num = (2, 3.4)
+function multiply(num1, num2) {
+  return (num1 * num2)
+}
+
+var num = (5.0, 2.5)
+function divide(num1, num2) {
+  return (num1 / num2)
+}
+
+var num = (5)
+function inc(num1) {
+return (num1 + 1)
+}
+
+var num = (10)
+function dec(num1) {
+  return (num1 - 1)
+}
+
+function makeInt (n) {
+  return parseInt(n, 10)
+  console.log(n)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n, 10)
+  console.log(n)
+}
