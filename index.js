a62230378b14f85d3ae6f689fbc83ed34b375e9d
Roberta Munive <ramunive@gmail.com>
2020-02-04 19:23:38 +0000

    Done.

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ function decrement(n){
   return n -= 1;
 }
 
-function makeInt(n){
-  return parseInt(n, 10);
+function makeInt(string){
+  return parseInt(string, 10);
 }
 
-function preserveDecimal(n){
-  return parseFloat(n);
+function preserveDecimal(string){
+  return parseFloat(string);
 }
