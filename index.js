774aed9e3b1e16744b3b45622412e585f20f6e83
Liridona Morina <liridonamorina00@gmail.com>
2020-02-12 18:18:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+
+function preserveDecimal(string){
+  return parseFloat(string)
+}
+function makeInt(string){
+  return parseInt(string, 10)
+}
+function add(a,b){
+  return a+b;
+}
+function subtract(a,b){
+  return a-b
+}
+function multiply(a,b){
+  return a*b
+}
+function divide(a,b){
+  return a/b
+}
+function increment(n)
+{
+  return n++
+}
+funciton decrement(n)
+{
+  return n--
+}
