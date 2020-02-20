7db7e7e4523c8663e1b94c60297d341483fdc733
Chana Kupetz <chanakupetz@gmail.com>
2020-02-20 15:36:47 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function increment(n) {
+  return +=n
+}
+
+/* function decrement(n) {
+
+}
+Okay, now we're ready to write solutions for the next two functions:
+increment(n) and decrement(n).
+These methods should take in a number,
+and either increments the provided value by one or decrements
+it by one respectively,
+returning the result.
+*/
