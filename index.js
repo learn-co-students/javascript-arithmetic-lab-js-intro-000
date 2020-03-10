b867bac8b57668ee253f6e6ea59e4099d0fd1a3d
Luis Hawkins <bladerunnerluis@gmail.com>
2020-03-10 02:04:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(x, y) {
+  return  x + y
+}
+function subtract(x, y) {
+  return x - y
+}
+function divide(x, y) {
+  return x/y
+}
+
+function multiply(x, y)  {
+
+  return x * y
+}
+
+
+function increment(number) {
+  number ++
+  return number
+}
+
+function decrement(number){
+  number --
+  return number
+}
+
+function makeInt(string) {
+return  parseInt(string, 10)
+}
+
+function preserveDecimal(string) {
+  return parseFloat(string)
+}
