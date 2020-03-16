c6cf3718f1f7ddadcd6b4b2e42ac8a75e4bfa833
Benjamin Cohn <cohnbj13@gmail.com>
2020-03-16 23:27:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(n1,n2) {
+  return n1+n2
+}
+
+function subtract(n1,n2) {
+  return n1-n2
+}
+
+function multiply(n1,n2) {
+  return n1*n2
+}
+
+function divide(n1,n2) {
+  return n1/n2
+}
+
+function increment(n) {
+  return ++n
+}
+function decrement(n) {
+  return --n
+
+}
+function makeInt(string) {
+  return parseInt(string,10)
+}
+
+function preserveDecimal(string) {
+  return parseFloat(string)
+}
