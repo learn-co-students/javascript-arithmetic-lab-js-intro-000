3497081c723b8ba473a1ea70b8c7d1a4c9b47795
Wei Xin Koh <e0201350@u.nus.edu>
2020-03-27 17:01:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(num1, num2) {
+  return num1 + num2
+}
+
+function subtract(num1, num2) {
+  return num1 - num2
+}
+
+function multiply(num1, num2) {
+  return num1 * num2
+}
+
+function divide(num1, num2) {
+  return num1 / num2
+}
+
+function increment(num) {
+  return ++num
+}
+
+function decrement(num) {
+  return --num
+}
+
+function makeInt(string) {
+  return parseInt(string, 10)
+}
+
+function preserveDecimal(string) {
+  return parseFloat(string)
+}
