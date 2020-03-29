c0f2e187843189e2b85a0bf8e077ab7bc58edec9
NICHOLAS R VANDERMOLEN <tree.vandermolen@gmail.com>
2020-03-29 01:37:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(){
+
+}
+function subtract(){
+
+}
+function multiple(){
+
+}
+function divide(){
+
+}
+function add(a, b){
+  return a+b;
+}
+function subtract(a, b){
+  return a-b;
+}
+function multiply(a, b){
+  return a*b;
+}
+function divide(a, b){
+  return a/b;
+}
+function increment(n){
+  return +=;
+}
+function decrement(n){
+  return -=;
+}
+function
