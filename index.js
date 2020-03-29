22f0319930608a6007323576ff3e4e0e27a6fdc8
Joseph Carson <josephc89@fullsail.edu>
2020-03-29 19:20:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+return a + b
+}
+
+function subtract(a, b) {
+return a - b
+}
+
+function multiply(a, b) {
+return a * b
+}
+
+function divide(a, b) {
+return a / b
+}
+
+function increment(n) {
+return n += 1
+}
+
+function decrement(n) {
+return n -= 1
+}
+
+function makeInt(string) {
+  parseInt('2', 10)
+  return parseInt(string, 10)
+}
+
+function preserveDecimal(a) {
+  parseFloat('2.222')
+  return parseFloat(a)
+}
