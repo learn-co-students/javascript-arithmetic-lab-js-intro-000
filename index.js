a89487335e50ea503349df0effe396fa4da70b39
Illia Brytun <illiabrytun@gmail.com>
2020-04-02 21:36:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,41 @@
+function add() {
+}
+
+function subtract() {
+}
+
+function multiple() {}
+
+function divide() {}
+
+function increment(n) {
+  return n+=1
+}
+
+function decrement(n) {
+  return n-=1
+}
+
+function add(a,b) {
+  return a+b
+}
+
+function subtract(a,b) {
+  return a-b
+}
+
+function multiply(a,b) {
+  return a*b
+}
+
+function divide(a,b) {
+  return a/b
+}
+
+function makeInt(string) {
+  return parseInt(string, 10)
+}
+
+function preserveDecimal(string) {
+  return parseFloat(string)
+}
