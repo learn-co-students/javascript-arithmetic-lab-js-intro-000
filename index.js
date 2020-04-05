a8cc4f6d9e3bb5edc315917eb3fec42b14a8b8fd
Noa'man Patel <normz@live.co.uk>
2020-04-05 20:17:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,53 +1,31 @@
-let a = Math.floor(Math.random() * 1000)
-let b = Math.floor(Math.random() * 1000)
-function add(a, b){
-return a+b;
+function add(a,b){
+  return a + b;
 }
-add(a, b);
 
-let c = Math.floor(Math.random() * 1000)
-let d = Math.floor(Math.random() * 1000)
-function subtract(c, d){
-return c-d;
+function subtract(a,b){
+  return a - b;
 }
-subtract(c, d);
 
-let e = Math.floor(Math.random() * 1000)
-let f = Math.floor(Math.random() * 1000)
-function multiply(e, f){
-return e*f;
+function multiply(a,b){
+  return a * b;
 }
-multiply(e, f);
 
-let g = Math.floor(Math.random() * 1000);
-let h = Math.floor(Math.random() * 1000);
-function divide(g, h){
-return g/h;
+function divide(a,b){
+  return a / b;
 }
-divide(g, h);
 
-
-
-let i = Math.floor(Math.random() * 1000);
-function inc(i){
-return i+1;
+function increment(n){
+  return n += 1;
 }
-inc(i);
 
-let j = Math.floor(Math.random() * 1000);
-function dec(j){
-return j-1;
+function decrement(n){
+  return n -= 1;
 }
-dec(j);
 
-let k = Math.floor(Math.random() * 1000);
-function makeInt(k){
-return parseInt(k, 10);
+function makeInt(n){
+  return parseInt(n, 10);
 }
-makeInt(k);
 
-let l = Math.floor(Math.random() * 1000);
-function preserveDecimal(l){
-return parseFloat(l, 10);
+function preserveDecimal(n){
+  return parseFloat(n);
 }
-preserveDecimal(l);
