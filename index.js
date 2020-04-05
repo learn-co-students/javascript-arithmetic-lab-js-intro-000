3095344a7085d3519bfa4587822d01b301372b49
Christoper Mercado <christoper_m15@yahoo.com>
2020-04-05 14:31:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(x,y) {
+return x+y
+}
+
+function subtract(x,y) {
+return x-y
+}
+
+function multiply(x,y) {
+return x*y
+}
+
+function divide(x,y) {
+return x/y
+}
+
+function increment(n) {
+n++
+return n
+}
+
+function decrement(n) {
+n--
+return n
+}
+
+function makeInt(string) {
+return parseInt(string, 10)
+}
+
+function preserveDecimal(string) {
+return  parseFloat(string)
+}
