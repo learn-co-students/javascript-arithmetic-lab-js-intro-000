1af6da175866653b7b0168f859624dc9b4d16b43
Sanjay Paul Charles <sanjaypaulcharles98@gmail.com>
2020-04-07 18:35:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(a,b){
+return(a+b)
+}
+function subtract(a,b){
+  return(a-b)
+}
+function multiply(a,b){
+  return(a*b)
+}
+function divide(a,b){
+  return(a/b)
+}
+function increment(n){
+  n++;
+  return(n)
+}
+function decrement(n){
+  n--;
+  return(n)
+}
+function makeInt(n){
+  parseInt(n,10)
+  return(parseInt(n,10))
+}
+function preserveDecimal(n){
+    return(parseFloat(n))
+}
