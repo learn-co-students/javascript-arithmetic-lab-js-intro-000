bb34924bfff0ca308e3acfb8bc0dd45f0fd59c1c
ineffable-coder-0435 <gerren62@gmail.com>
2020-04-24 15:55:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,49 @@
+var a=1, b=5
+
+function add (){
+}
+
+function subtract (){
+}
+
+function multiple (){
+}
+
+function divide (){
+}
+
+function add (a,b){
+return a + b
+}
+
+function subtract (a,b){
+return a - b
+}
+
+function multiply (a,b){
+return a * b
+}
+
+function divide (a,b){
+return a / b
+}
+
+function increment (n){
+n++
+return n
+}
+
+function decrement (n){
+n--
+return n
+}
+
+function makeInt (n){
+parseInt('n',10)
+return parseInt (n, 10)
+}
+
+function preserveDecimal(n){
+  parseFloat('n')
+  return parseFloat (n)
+}
