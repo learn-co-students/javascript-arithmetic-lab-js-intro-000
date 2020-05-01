a33be1655da9c323b3acc6c807a8cfbe29cb0881
Corey Lynch <bearinawolfpack@gmail.com>
2020-05-01 16:09:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+
+function add(a, b) {
+    return (a + b);
+}
+
+function subtract(a, b) {
+    return (a - b);
+}
+
+function multiply(a, b) {
+    return (a * b);
+}
+
+function divide(a, b) {
+    return (a / b);
+}
+
+function increment(a) {
+    return a += 1;
+}
+
+function decrement(a) {
+    return a -= 1;
+}
+
+function makeInt(a) {
+    var num = parseInt(a, 10);
+        return(num);
+}
+
+function preserveDecimal(a) {
+    var num = parseFloat(a);
+        return(num);
+}
