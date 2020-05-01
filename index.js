749253265c2f36b07fb27485f7e47c9e5ca16138
Alexander kagerer <kagerer19@gmail.com>
2020-05-01 15:32:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,44 @@
+function add(a, b) {
+  
+}
+function add(first, second) {
+  var total = first + second;
+  return total;
+  
+}
+function subtract(first, second) {
+  var total = first - second;
+  return total;
+  
+}
+function multiply(first, second) {
+  var total = first * second;
+  return total;
+ 
+}
+function divide(a, b) {
+  var total = a / b;
+  return total;
+  
+}
+function increment(n) {
+  var total =   ++n;
+  return total;
+  
+}
+function decrement(n) {
+  var total = --n;
+  return total;
+  
+}
+
+
+function makeInt(n, base) {
+   var makeInt = parseInt(n, 10);
+   return makeInt;
+}
+function preserveDecimal(n, base) {
+  var preserveDecimal =  parseFloat(n, true)
+  return preserveDecimal;
+}
+
