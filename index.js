ea525f385bf22d47a49a4d2f583efc70d1094130
Brett Wilson <wilson.3458@osu.edu>
2020-05-04 07:15:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+// Adds inputs:
+function add(a,b){
+  return a + b;
+}
+// Subtracts b from a:
+function subtract(a,b){
+  return a - b;
+}
+// Divides a by b:
+function divide(a,b){
+  return a / b;
+}
+// Multiplies inputs:
+function multiply(a,b){
+  return a * b;
+}
+// Increases input by 1:
+function increment(n){
+  n++;
+  return n;
+}
+// Decreases input by 1:
+function decrement(n){
+  n--;
+  return n;
+}
+// Parses the input string to an integer:
+function makeInt(string){
+  return parseInt(string, 10);
+}
+// Parses the input string to a float:
+function preserveDecimal(string){
+  return parseFloat(string, 10);
+}
