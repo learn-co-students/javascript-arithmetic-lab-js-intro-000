d118e3abeaa55ca48309a1d67e684ee13e03c5ec
IMOH SILAS <imosil34@gmail.com>
2020-05-06 17:11:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,42 @@
+function add(){
+  return
+}
+function subtract(){
+  return
+}
+function multiply(){
+  return
+}
+function divide(){
+  return
+}
+function add(a, b){
+  return (a+b);
+}
+function subtract(a, b){
+  return (a-b);
+}
+function multiply(a, b){
+  return (a*b);
+}
+
+
+function divide(a, b){
+
+  return (a/b);
+
+}
+function increment(n){
+n++
+  return n;
+}
+function decrement(n){
+  n--;
+  return n;
+}
+ function makeInt(n){
+   return parseInt(n, 10);
+ }
+ function preserveDecimal(n){
+  return parseFloat(n,10);
+}
