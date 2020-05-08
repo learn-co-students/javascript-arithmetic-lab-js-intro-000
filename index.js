44a71402e0c99838209d521b52dd274039fe12cb
Daniel Do <daniel.minh.do@gmail.com>
2020-05-08 04:00:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+var number = 10
+
+function add(value)
+  {number += value}
+
+function subtract(value)
+  {number -= value}
+
+function multiple(value)
+  {number *= value}
+
+function divide(value)
+  {number /= value}
+
+function add(a, b)
+  {return a += b}
+
+function subtract(a, b)
+  {return a -= b}
+
+function multiply(a, b)
+  {return a *= b}
+
+function divide (a, b)
+  {return a /= b}
+
+function increment(n)
+  {return n + 1}
+
+function decrement(n)
+  {return n - 1}
+
+function makeInt(n)
+  {return parseInt(n, 10)}  // only returns integers
+
+function preserveDecimal(n)
+  {return parseFloat(n)}    // preserves decimals
