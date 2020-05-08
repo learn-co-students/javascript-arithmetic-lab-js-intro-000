2f9c4e52d866ea7a16db2ad9e7387193f59a5aa9
Melissa Heying <melissakheying@gmail.com>
2020-05-08 16:12:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,49 @@
+function makeInt(string) {
+  parseInt('string', 10);
+}
+
+function preserveDecimal(string) {
+  parseFloat('string', 10);
+}
+
+function add(string) {
+  parseInt('string' + 10);
+}
+function subtract(string) {
+  parseInt('string' - 10);
+}
+function multiply(string) {
+  parseInt('string' * 10);
+}
+
+function divide(string) {
+  parseInt('string' / 10);
+}
+
+function add(a,b) {
+  return a + b;
+}
+function subtract(a,b) {
+  return a - b;
+}
+
+function multiply(a,b) {
+  return a * b;
+}
+function divide(a,b) {
+  return a / b;
+}
+
+function increment(n) {
+  return ++n;
+}
+function decrement(n) {
+  return --n;
+}
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n, 10);
+}
