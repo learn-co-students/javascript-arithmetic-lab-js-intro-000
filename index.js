5fee33975542afbe387a28deeba513d01126ee11
Ruth Vera <rvamore89@gmail.com>
2020-05-12 20:09:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(){}
+function subtract(){}
+function multiple(){}
+function divide(){}
+var a, b
+function add(a, b){
+  return (a + b)
+}
+function subtract(a, b){
+  return (a - b)
+}
+function multiply(a, b){
+  return (a * b)
+}
+function divide(a, b){
+  return (a / b)
+}
+
+function increment(n){
+ return (n += 1)
+}
+
+
+function decrement(n){
+ return (n -= 1 )
+}
+
+
+function makeInt(n){
+  return parseInt(n, 10 )
+
+}
+
+function preserveDecimal(n){
+  return parseFloat(n)
+
+}
