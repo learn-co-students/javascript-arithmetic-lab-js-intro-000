cc1a31474fabccbbd53d6c0216b042b0ea6a064b
dadoris <dadoris@uw.edu>
2020-05-14 03:58:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(number1, number2){
+  return number1 + number2
+}
+
+function subtract(number1, number2){
+  return number1 - number2
+}
+
+function multiply(number1, number2){
+  return number1 * number2
+}
+
+function divide(number1, number2){
+  return number1 / number2
+}
+
+function increment(n){
+  return ++n
+}
+
+
+function decrement(n){
+  return --n
+}
+
+function makeInt(str){
+  return parseInt(str,10)
+}
+
+function preserveDecimal(str){
+  return parseFloat(str)
+}
