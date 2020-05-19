61f7cbef7102816a0abd7faad9ee9fc086be493d
Ismaiilov Atai <atai2497@gmail.com>
2020-05-19 21:37:57 +0000

    Done.

--- index.js
+++ index.js
@@ -2,9 +2,42 @@ function add(x, y) {
   return x + y;
 }
 add(10,10);
-function substract() {
-var a = 10
-a -= 10
+function subtract(a, b) {
+  a -= b
+
 return a
 }
-substract(10);
+subtract(520, 1);
+
+function multiply(a, b) {
+  return a * b;
+}
+multiply(10, 20)
+ function divide(a, b) {
+   a /= b
+   return a;
+ }
+ divide(15, 5);
+
+ function increment(n) {
+   ++n;
+   return n;
+ }
+increment(100)
+
+function decrement(n) {
+  --n;
+  return n;
+}
+decrement(200);
+
+function makeInt(n){
+
+  return parseInt(n, 10);
+}
+makeInt('4', 10);
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
+preserveDecimal('124.143,13')
