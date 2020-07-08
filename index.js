5bf92cd656db9b98ea3e6979bbebdf21225dffb2
Ekambeer Singh <akashdeep0406@gmail.com>
2020-07-08 13:43:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(x,y){
+  return x+y;
+}
+function subtract(a,b){
+  return a-b;
+}
+function multiply(a,b){
+  return a*b;
+}
+function divide(a,b){
+  return a/b;
+}
+function increment(n){
+  return (n+=1);
+}
+function decrement(n){
+  return (n-=1);
+}
+function makeInt(n){
+  return parseInt(n,10);
+}
+function preserveDecimal(n){
+  return parseFloat(n);
+}
