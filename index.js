080a85e040509fa45743f109db48f994d425e6fc
Eric C <erix2205@gmail.com>
2020-07-09 10:48:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(number1,number2){
+  return number1 + number2
+}
+
+function subtract(a,b){
+  return a-b
+}
+
+function multiply(a,b){
+  return a*b
+}
+
+function divide(a,b){
+  return a/b
+}
+
+function increment(n){
+  return n+=1
+}
+
+function decrement(n){
+  return n-=1
+}
+
+// makeInt()
+function makeInt(a){
+  return parseInt(a,10)
+}
+
+function preserveDecimal(a){
+  return parseFloat(a)
+}
