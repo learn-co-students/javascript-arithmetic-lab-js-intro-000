99d89036e9a559b4ae4aeca6e2d7644a41167af2
Cole Cezeaux <colecezeaux@gmail.com>
2020-07-22 16:51:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function add() {
+add(1 + 80) // 81
+}
+function subtract() {
+subtract(60 - 40) // 20
+}
+function multiply() {
+multiply(2 * 3.4) // 6.8 (there's that floating-point arithmetic again...)
+}
+function divide() {
+divide(5.0 / 2.5) // 2
+}
+
+var number = 500
+function add() {
+  add(number += 467)
+}
