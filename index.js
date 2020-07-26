1c4923f0c519d5c64e6442418b8110fedb70bd3e
Edgaras Narbutas <edgar.narbutas@siu.edu>
2020-07-26 19:27:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+function add() {5+5}
+function subtract() {5-5}
+function multiply() {5*5}
+function divide() {5/5}
+
+function add(a, b) {
+   return a + b
+}
+
+function subtract(a,b) {
+  return a - b 
+}
+
+function multiply(a,b) {
+  return a * b 
+}
+
+function divide(a,b) { 
+  return a / b 
+}
+
+function increment(n) {
+  return ++n 
+}
+
+function decrement(n) {
+  return --n 
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
