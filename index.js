55d5c6bf3b513d066b055c22e0d546f68111cb29
Matthew Ogtong <ogtongm@gmail.com>
2020-07-30 21:50:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(first, second) {
+  return first + second
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function increment(a) {
+  a++
+  return a
+}
+
+function decrement(a) {
+  a--
+  return a
+}
+
+function makeInt(a) {
+  return parseInt(a, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
