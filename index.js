6606ae94fe4884955d4392920fb5c9d34fd5a028
practical-trainer-5867 <misterjcr@aol.com>
2020-08-04 06:47:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function add(a,b) {
+  return a + b
+}
+
+function subtract(a,b) {
+  return a - b
+}
+
+function multiply(a,b) {
+  return a * b
+}
+
+function divide(a,b) {
+  return a / b
+}
+
+function increment(n) {
+  return(++n)
+}
+
+function decrement(n) {
+  return(--n)
+}
+function makeInt(string) {
+  return parseFloat(string)
+}
+function preserveDecimal(string) {
+  return parseFloat(string)
+}
