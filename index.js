f20fee32112807ee79467aaec158053422bd930c
Grant Stafford-Renard <gstaffordrenard@gmail.com>
2020-08-04 18:21:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function add() {
+700 + 47
+}
+function subtract() {
+1 - 1
+}
+function multiply() {
+1 * 1
+}
+function divide() {
+1 / 1
+}
+
+var n = 5
+
+function increment(n) {
+return('${n}' += '58')
+}
+function decrement(n) {
+
+}
+
+
+function add(a, b) {
+700 + 47
+}
+function subtract(a, b) {
+1 - 1
+}
+function multiply(a, b) {
+1 * 1
+}
+function divide(a, b) {
+1 / 1
+}
