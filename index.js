1f1123b999343a7a57d3c214523de0f34fb75691
Oleksandr <bilovka10@gmail.com>
2020-08-07 17:09:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(){
+  
+}
+function substract(){
+  
+}
+function add(a,b){
+  return a+b
+}
+function subtract(a,b){
+  return a-b
+}
+function multiply(a,b){
+  return (a*b)
+}
+function divide(a,b){
+  return a/b
+}
+function increment(n){
+  return n+1
+}
+function decrement(n){
+  return n-1
+}
+function makeInt(n){
+  return parseInt(n, 10)
+  return parseInt('0x2328',10)
+  return parseInt('zzz',10)
+}
+function preserveDecimal(n){
+  return parseFloat(n)
+  return parseFloat('zzz')
+}
