0bcae90874f6b454016619218ed64362a6518d47
antonio allen <modusoul@gmail.com>
2020-08-13 21:01:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,60 @@
+let number= 10
+
+function add(string)
+{
+  var number= '10'
+
+  number += 5
+
+  console.log(string)
+}
+
+
+
+function add(a,b)
+{
+number = a + b
+return a + b
+}
+
+function subtract(a,b)
+{
+  number = a - b
+  return a - b
+}
+
+function multiply(a,b)
+{
+  number = a * b
+  return a * b
+}
+
+function divide(a,b)
+{
+  number = a / b
+  return a / b
+}
+
+function increment(n)
+{
+  number = n + 1
+  return number
+}
+
+function decrement(n)
+{
+  number = n - 1
+  return number
+}
+
+function makeInt(n)
+{
+  number = parseInt(n, 10)
+  return number
+}
+
+function preserveDecimal(n)
+{
+  number = parseFloat(n,10)
+  return number
+}
