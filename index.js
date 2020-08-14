69c1b93e8ac33f84b6dc81de475d4c8dfca990ce
alluring-machine-4767 <bgreiser@gmail.com>
2020-08-14 22:49:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a,b){
+  return a + b;
+}
+
+function subtract(a,b) {
+  return a - b;
+}
+
+function multiply(a,b) {
+  return a * b;
+}
+
+function divide(a,b) {
+  return a / b;
+}
+
+function makeInt(string) {
+  return parseInt(string, 10)
+}
+
+function preserveDecimal(string) {
+  return parseFloat(string);
+}
+
+function increment(n) {
+  let a = (n + 1);
+  return a;
+}
+
+function decrement(n) {
+  let a = (n - 1);
+  return a;
+}
