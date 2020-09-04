f84e6487b4e42af47623cd16489a77a260fb6e29
Cole Cezeaux <colecezeaux@gmail.com>
2020-09-04 15:39:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,40 +1,26 @@
-function add() {
-add(1 + 80) // 81
+function add(a,b) {
+  return a+b
 }
-function subtract() {
-subtract(60 - 40) // 20
+function subtract(a,b) {
+  return a-b
 }
-function multiply() {
-multiply(2 * 3.4) // 6.8 (there's that floating-point arithmetic again...)
+function multiply(a,b) {
+  return a*b
 }
-function divide() {
-divide(5.0 / 2.5) // 2
+function divide(a,b) {
+  return a/b
 }
-
-function add(a, b) {
-  return a + b
-}
-function subtract(a, b) {
-  return a - b
-}
-function multiply(a, b) {
-  return a * b
-}
-function divide(a, b) {
-  return a / b
-}
-
 function increment(n) {
-  return n += 1
+  n = n + 1
+  return n++
 }
 function decrement(n) {
-  return n -= 1
+  n = n - 1
+  return n--
 }
-
 function makeInt(n, base) {
   return parseInt(n, 10)
 }
-
 function preserveDecimal(n) {
-  return parseFloat(n)
+    return parseFloat(n)
 }
