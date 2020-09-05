89b3b259499ce7bd484e3e65965cac2763402892
Alexander Petrov <acepetrov96@gmail.com>
2020-09-05 16:07:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(){
+
+}
+function subtract(){
+
+}
+function multiply(){
+
+}
+function divide(){
+
+}
+function add(a,b){
+  return a+b;
+}
+function subtract(a,b){
+  return a-b;
+}
+function multiply(a,b){
+  return a*b;
+}
+function divide(a,b){
+  return a/b;
+}
+function increment(n){
+  return ++n;
+}
+function decrement(n){
+  return --n;
+}
+function makeInt(string){
+  return parseInt(string, 10);
+}
+function preserveDecimal(string){
+  return parseFloat(string);
+}
