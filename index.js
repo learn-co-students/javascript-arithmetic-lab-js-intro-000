325ae84a4744f1cb1d4a6c2b2c11dce271c143d8
Yuxiang (Sean) Li <seanliyuxiang@gmail.com>
2020-09-19 06:14:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a,b) {
+  return a + b;
+}
+
+function subtract(a,b) {
+  return a-b;
+}
+
+function multiply(a,b) {
+  return a*b;
+}
+
+function divide(a,b) {
+  return a/b;
+}
+
+function increment(n) {
+  // n+=1;
+  return n+=1;
+}
+
+function decrement(n) {
+  return n-=1;
+}
+
+function makeInt(str) {
+  return parseInt(str,10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
