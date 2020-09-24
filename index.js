89e54955b2b800ac49d58feaba45aaa202c21426
Cole Cezeaux <colecezeaux@gmail.com>
2020-09-24 17:08:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-function add(a, b) {
-  return a+b
+function add(a,b) {
+  return (a+b)
 }
 
-function subtract(a, b) {
-  return a-b
+function subtract(a,b){
+  return (a-b)
 }
 
-function multiply(a, b) {
-  return a*b
+function multiply(a,b) {
+  return (a*b)
 }
 
- function divide(a, b) {
-   return a/b
- }
+function divide(a,b) {
+  return (a/b)
+}
 
 function increment(n) {
-  n = n+1
-  return n
+  return n+=1
 }
 
 function decrement(n) {
-  n = n-1
-  return n
+  return n-=1
 }
 
 function makeInt(n) {
