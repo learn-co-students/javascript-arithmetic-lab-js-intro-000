2045a5507f930401ca10eb4919dec6cac4a01576
David Glading <davidjglading@gmail.com>
2020-10-16 18:10:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,55 @@
+
+
+
+
+function add(a, b) {
+  20 + 30
+  returns('20 + 30 = 50')
+}
+
+function subtract(a, b) {
+  30 - 20
+  returns('30 - 20 = 10')
+}
+
+function multiply(a, b) {
+  30 * 20
+  returns('30 * 20 = 600')
+}
+
+function divide(a, b) {
+  600 / 30
+  returns('600 / 30 = 20')
+}
+
+function increment(num) {
+  return ++num
+}
+
+function decrement(num) {
+  return --num
+}
+
+function makeInt(string) {
+  return parseInt(string, 10)
+}
+
+function preserveDecimal(string) {
+  return parseFloat(string)
+}
+
+function add(num1, num2) {
+  return num1 + num2
+}
+
+function subtract(num1, num2) {
+  return num1 - num2
+}
+
+function multiply(num1, num2) {
+  return num1 * num2
+}
+
+function divide(num1, num2) {
+  return num1 / num2
+}
