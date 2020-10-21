5dbc28b2d9e3f175b81315ef9b66b2eff8f8195d
Joshua Ramos <bassramos@gmail.com>
2020-10-21 03:16:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function makeInt(string)  {
+    parseInt(string)
+    return parseInt(string, 10)
+}
+function preserveDecimal(string)  {
+    parseFloat(string)
+    return parseFloat(string)
+}
+function add(a, b)  {
+    return a + b
+}
+function subtract(a, b)   {
+    return a - b
+}
+function multiply(a, b) {
+    return a * b
+}
+function divide(a, b) {
+    return a / b
+}
+function increment(number)  {
+  return ++number
+}
+function decrement(number)  {
+    return --number
+}
