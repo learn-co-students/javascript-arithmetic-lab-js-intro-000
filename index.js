2e32cf3a382c9053a7c40dc931c257d1c6abaf89
witty-abstraction-1855 <merve.kastan4@gmail.com>
2020-10-30 20:24:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,55 @@
+function add(a,b) {
+  console.log(a+b);
+}
+function subtract(a,b) {
+  console.log(a-b)
+}
+function multiply(a,b){
+  console.log(a*b)
+}
+function divide(a,b) {
+  console.log(a/b)
+}
+function add(a,b) {
+  return (a+=b)
+}
+
+function subtract(a,b) {
+  return(a-=b)
+}
+function multiply(a,b) {
+  return (a*b)
+}
+
+function divide(a,b) {
+  return (a/=b)
+}
+
+
+function increment(n){
+  console.log(n++);
+  return n
+}
+function decrement(n) {
+  console.log(n--)
+  return n
+}
+
+function makeInt(n) {
+  console.log(parseInt(n))
+  return n
+
+}
+function makeInt(n) {
+  console.log(parseInt(n,10));
+
+}
+function preserveDecimal(n) {
+  var n=parseFloat(n)
+return n
+
+}
+function preserveDecimal(n) {
+  console.log(parseInt("hey"))
+  return
+}
