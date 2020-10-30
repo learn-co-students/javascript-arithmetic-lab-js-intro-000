57cde15a9c934bc2c94cd9596c274c43068cf43c
Dashawn <dashawnhm@gmail.com>
2020-10-30 21:11:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(a,b){
+  return a+b
+}
+function subtract(a,b){
+  return a-b
+}
+function multiply(a,b){
+  return a*b
+}
+function divide(a,b){
+  return a/b
+}
+function increment(n){
+  return n+=1
+}
+function decrement(n){
+  return n-=1
+}
+function makeInt(string){parseInt('n',10)
+return parseInt(string,10)
+}
+function preserveDecimal(string){parseFloat('n')
+return parseFloat(string)
+}
