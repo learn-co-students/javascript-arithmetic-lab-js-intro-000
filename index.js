c30522e2b75e07200fee291109d90ba042bd1db9
cerulean-revision-6689 <jordanjennings12@gmail.com>
2020-11-13 01:25:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,49 @@
+function add(){
+  a + b
+}
+
+function subtract(){
+  a - b
+}
+
+function multiply(){
+  a * b
+}
+
+function divide(){
+  a / b
+}
+
+function add(a, b){
+  return a + b
+}
+
+function subtract(a, b){
+  return a - b
+}
+
+function multiply(a, b){
+  return a * b
+}
+
+function divide(a, b){
+  return a / b
+}
+
+function increment(n){
+  n++
+  return n
+}
+
+function decrement(n){
+n--
+return n
+}
+
+function makeInt(n){
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n){
+  return parseFloat(n);
+}
