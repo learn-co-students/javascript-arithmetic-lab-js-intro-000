a4c5c7cbf4d846c6f04130a18e63919e481eab26
Amanda Nikrant <ajnikrant@gmail.com>
2020-11-21 22:37:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,54 @@
+function add() {
+  return 4 + 968;
+}
+
+function subtract() {
+  return 33330-44;
+}
+
+function multiply() {
+  return 55*5;
+}
+
+function divide() {
+  return 100/10;
+}
+
+function add(a, b) {
+  return a+=b;
+}
+
+function subtract(a, b) {
+  return a-=b;
+}
+
+function multiply(a, b) {
+  return a*=b;
+}
+
+function divide(a, b) {
+  return a/=b;
+}
+
+function increment(n) {
+  ++n;
+  return n;
+}
+
+
+function decrement(n) {
+  --n;
+  return n;
+}
+
+
+function makeInt(n) {
+  n = parseInt(n, 10);
+  return n;
+}
+
+
+function preserveDecimal(n) {
+  n = parseFloat (n);
+   return n;
+}
