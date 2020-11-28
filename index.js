bb64a84fed1d33c5fe8913685afc4bc87addb551
sunam RImal Nepali <tsunamr@gmail.com>
2020-11-28 17:01:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(number){
+  return number
+}
+function subtract(number){
+  return number
+}
+function multiply(number){
+  return number
+}
+function divide(number){
+  return number
+}
+function add(a,b){
+  return (a+b)
+}
+function subtract(a,b){
+  return (a-b)
+}
+function multiply(a,b){
+  return (a*b)
+}
+function divide(a,b){
+  return (a/b)
+}
+function increment(n){
+  return ++n
+}
+function decrement(n){
+  return --n
+}
+
+function makeInt(n){
+  parseInt (n, 10)
+  return parseInt(n, 10)
+}
+function preserveDecimal(n){
+  parseFloat(n)
+  return parseFloat(n)
+}
