99f334e66721760b85636395c109bdead1bf287c
Omar Razi <omarrazi0425@yahoo.com>
2020-12-08 19:02:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a,b) {
+return a + b
+}
+function subtract(a,b) {
+  return a - b
+}
+function multiply(a,b) {
+  return a * b
+}
+function divide(a,b) {
+  return a / b
+  }
+
+function increment(n) {
+  n++;
+  return n
+}
+
+function decrement(n) {
+  n--;
+  return n
+}
+
+function makeInt(a,b) {
+  return parseInt(a,10);
+}
+
+function preserveDecimal(a,b) {
+return parseFloat(a,10);
+
+}
