37f597ab6e2a0d20d714cd0b367221c926970ed9
Lupita Rivera <lupitarivera8899@gmail.com>
2020-12-29 21:06:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,47 @@
+var number;
+function add() {1+8}
+function subtract() {60-40}
+function multiply() {2* 3.4}
+function divide() {5.0 / 2.5}
+function add(a,b) {return a+b}
+function subtract(a,b){return a-b}
+function multiply(a,b){return a*b}
+function divide(a,b) {return a/b}
+
+function inc(n){ //Increment a by 1 and return the result.
+  number = n + 1;
+  return number;
+}
+function dec(n){ //Decrement a by 1 and return the result.
+  number = n- 1;
+  return number;
+}
+function makeInt(n){ //Parse a as an integer and return that integer.
+  number = parseInt(n, 10);
+  return number;
+}
+function preserveDecimal(n){ //Parse a as a float (number with decimals) and return that number.
+  number = parseFloat(n, 10);
+  return number;
+}
+
+
+function decreament() {var n=0;
+n--;
+return n;}
+
+var i = 0;
+
+function increment(n){
+
+  n++;
+  return n;
+}
+
+var n = 0;
+
+function decrement(n){
+
+  n--;
+  return n;
+}
