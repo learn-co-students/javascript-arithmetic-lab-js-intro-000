0d585ffefeeb617d648e99d5d4374b1b9d9a3567
Wilson <karaewilson16@gmail.com>
2021-01-08 14:58:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,48 @@
+function add() {
+  number + 5
+}
+
+function subtract() {
+  number - 2
+}
+
+function multiply() {
+  number * 10
+}
+
+function divide() {
+  number / 4
+}
+
+function add(a, b) {
+  return(a + b)
+}
+
+function subtract(a, b) {
+  return(a - b)
+}
+
+function multiply(a, b) {
+  return(a*b)
+}
+
+function divide(a, b) {
+  return(a / b)
+}
+
+function increment(n){
+  return (++n)
+}
+
+function decrement(n){
+  return(--n)
+}
+
+function makeInt(n) {
+  return (parseInt(n, 10))
+}
+
+function preserveDecimal(string) {
+  let myStrFloat = 2.222
+  return (parseFloat(string))
+}
