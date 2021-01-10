1a7f1a8bb7cb85f178cb3e72af00a02293666c06
Aaron Lutas <alutas95@gmail.com>
2021-01-10 23:01:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function add(1, 80) {
+  number = 1 + 80
+  return number;
+}
+function subtract(60, 40) {
+  number = 60 - 40
+  return number;
+}
+function multiply(2, 3.4) {
+  number = 2 * 3.4
+  return number;
+}
+function divide(5.2, 2.5) {
+  number = 5.2 / 2.5
+  return number;
+}
+function makeInt(string) {
+  parseInt(string)
+}
+
+function preserveDecimal(string) {
+  parseFloat(string)
+}
