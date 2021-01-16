97ee2abc9acaf58bff9346be01fc8976750d068c
Noor Albayaty <noor.albayaty90@gmail.com>
2021-01-16 21:42:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,49 @@
+function add()
+{
+  var x= 5;
+  x+=5;
+  console.log(x);
+}
+function multiply()
+{
+  var x= 5;
+  x*=10;
+  console.log(x);
+}
+function subtract()
+{
+  var x= 5;
+  x-=10;
+  console.log(x);
+}
+function divide()
+{
+  var x= 20;
+  x/=5;
+  console.log(x);
+}
+function add(a,b)
+{
+  return a+b;
+}
+function subtract(a,b){
+  return a-b;
+}
+ function multiply(a,b){
+   return a*b;
+ }
+ function divide(a,b){
+   return a/b;
+ }
+ function  increment(x){
+   return ++x;
+ }
+ function decrement(x){
+   return --x;
+ }
+ function makeInt(x){
+   return parseInt(x, 10);
+ }
+function preserveDecimal(n){
+  return parseFloat(n);
+}
