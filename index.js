03db23dc51d499663db93dd3f71fe197e560381f
Amedeo d'Amore <amedeodamore@gmail.com>
2021-01-21 23:24:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,49 @@
+function add(){
+  1 + 2
+}
+
+function subtract(){
+  1 - 2
+}
+
+function multiply(){
+  1 * 2
+}
+
+function divide(){
+  1 / 2
+}
+
+function add(a,b){
+  return a + b
+}
+
+function subtract(a,b){
+return a - b
+}
+
+function multiply(a,b){
+return a * b
+}
+
+function divide(a,b){
+return a / b
+}
+
+function increment(n){
+return n += 1
+}
+
+function decrement(n){
+return n -= 1
+}
+
+function makeInt(n){
+return parseInt(n, 10)
+
+}
+
+function preserveDecimal(n){
+return parseFloat(n)
+
+}
