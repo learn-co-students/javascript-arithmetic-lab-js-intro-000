dc2898f078a03aa2ab4aed57aef7ddd572d122f7
Allison Levine <allison.s.levine@gmail.com>
2021-02-02 03:58:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+
+function add(a, b) {
+  add = (a + b);
+  return add
+}
+
+function subtract(a, b) {
+  subtract = (a - b);
+  return subtract
+}
+
+function multiply(a, b) {
+  multiply = (a * b);
+  return multiply
+}
+
+function divide(a, b) {
+  divide = (a / b);
+  return divide
+}
+
+function increment(n) {
+  var increment = n += 1;
+  return increment
+}
+
+function decrement(n) {
+  var decrement = n -= 1;
+  return decrement
+}
+
+function makeInt(n) {
+  var makeInt = parseInt(n, 10);
+  return makeInt
+}
+
+function preserveDecimal(n) {
+  var preserveDecimal = parseFloat(n);
+  return preserveDecimal
+}
