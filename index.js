518156f4f3d81501500cb7738b641929a6644aac
Caleb Roberts <calebtakespics@gmail.com>
2021-02-06 18:23:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a, b){
+  return a + b
+}
+
+function subtract(a, b){
+  return a - b
+}
+
+function multiply(a, b){
+  return a * b
+}
+function divide(a, b){
+  return a / b
+}
+
+function increment(a){
+  return a + 1
+}
+
+function decrement(a){
+  return a - 1
+}
+
+function makeInt(a){
+  return parseInt(a.toString(), 10)
+}
+
+function preserveDecimal(a){
+  return parseFloat(a.toString());
+}
