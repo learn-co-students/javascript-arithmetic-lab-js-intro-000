a491397a139d6e4509aee3165f8f3d4e0b160b53
Jake Houston <jakechouston@gmail.com>
2021-02-08 01:03:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(num1, num2) {
+  return num1 += num2
+}
+
+function subtract(num1, num2) {
+  return num1 -= num2
+}
+
+function multiply(num1, num2) {
+  return num1 *= num2
+}
+
+function divide(num1, num2) {
+  return num1 /= num2
+}
+
+function increment(n) {
+  n++;
+  return n;
+}
+
+function decrement(n) {
+  n--;
+  return n;
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
