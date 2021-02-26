eac95fadfba796e539a34b3b1ecd6f3d319c94b2
Dilovar Mamadov <d.mamadov@gmail.com>
2021-02-26 01:55:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,75 +1,24 @@
-var a, b
 
-beforeEach(function() {
-  a = Math.floor(Math.random() * 1000)
-  b = Math.floor(Math.random() * 1000)
-})
+function add(a, b) {
+  return a + b
+}
 
-describe('basic math functions', function () {
-  it("'add()' is a valid function", function() {
-    expect(a).toExist
-  })
+function subtract(a, b) {
+  return a - b
+}
 
-  it("'subtract()' is a valid function", function() {
-    expect(b).toExist
-  })
+function multiply(a, b) {
+  return a* b
+}
 
-  it("'multiply()' is a valid function", function() {
-    expect(a).toExist
-  })
+function divide(a, b) {
+  return a / b
+}
 
-  it("'divide()' is a valid function", function() {
-    expect(b).toExist
-  })
+function increment(n) {
+  return n + 1
+}
 
-  it('add(a, b) adds two numbers and returns the result', function() {
-    expect(add(a, b)).toEqual(a + b)
-  })
-
-  it('subtract(a, b) subtracts b from a and returns the result', function() {
-    expect(subtract(a, b)).toEqual(a - b)
-  })
-
-  it('multiply(a, b) multiplies two numbers and returns the result', function() {
-    expect(multiply(a, b)).toEqual(a * b)
-  })
-
-  it('divide(a, b) divides a by b and returns the result', function() {
-    expect(divide(a, b)).toEqual(a / b)
-  })
-
-  it('increment(n) increments n and returns the result', function() {
-    expect(increment(a)).toEqual(a + 1)
-  })
-
-  it('decrement(n) decrements n and returns the result', function() {
-    expect(decrement(a)).toEqual(a - 1)
-  })
-})
-
-
-
-
-describe('makeInt(n)', function() {
-  it('parses n as an integer and returns the parsed integer', function() {
-    expect(makeInt(a.toString())).toEqual(a)
-  })
-
-  it('assumes base 10', function() {
-    expect(makeInt('0x2328')).toEqual(0)
-  })
-
-  it('returns NaN as appropriate', function() {
-    expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
-  })
-})
-
-describe('preserveDecimal(n)', function() {
-  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
-    expect(preserveDecimal('2.222')).toBe(2.222)
-  })
-
-  it('returns NaN as appropriate', function() {
-    expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
-  })
-})
+function decrement(n) {
+  return n - 1
+}
