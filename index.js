a0762a893cef1ec26a7c8621a88e9cb9dfdd20ff
Francisco trejo <f85.trejo@gmail.com>
2021-03-01 01:43:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function add() {
+  return '+'
+}
+
+function subtract() {
+  return '-'
+}
+
+function multiply() {
+  return '*'
+}
+
+function divide() {
+  return '/'
+}
+
+function addNumbers(a, b) {
+  var     = a+b;
+return  '1511'
+
+}
