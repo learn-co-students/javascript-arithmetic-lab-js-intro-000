14e8cae02e94b71efe284f8d70a4c402ac495964
Francisco trejo <f85.trejo@gmail.com>
2021-03-01 02:11:54 +0000

    Done.

--- index.js
+++ index.js
@@ -14,8 +14,34 @@ function divide() {
   return '/'
 }
 
-function addNumbers(a, b) {
-  var     = a+b;
-return  '1511'
+function add(a,b) {
+  return a + b
+}
 
+function subtract(a,b) {
+  return a - b
+}
+
+function divide(a,b) {
+  return a / b
+}
+
+function multiply(a,b) {
+  return a * b
+}
+
+function increment(n) {
+  return n += 1
+}
+
+function decrement(n) {
+  return n -= 1
+}
+
+function makeInt(string) {
+  return parseInt(string, 10)
+}
+
+function preserveDecimal(string) {
+  return parseFloat(string)
 }
