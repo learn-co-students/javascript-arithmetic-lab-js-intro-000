1da1dd3cedb08e96525e7790a720ba7f661b7b88
yvonneeeee <yvonnelin840701@gmail.com>
2021-03-08 16:16:25 +0000

    Done.

--- index.js
+++ index.js
@@ -16,17 +16,23 @@ function divide(a,b){
 }
 
 function increment(n){
-  return (n += 1)
+  return (n += 1) //n++ || ++n
 }
 
 function decrement(n){
-  return (n -= 1)
+  return (n -= 1) //n-- || --n
 }
 
 function makeInt(n){
-  return (parseInt(n,10))
+  return parseInt(n,10)
+}
+function makeInt(string){
+  return parseInt(string,10)
 }
 
 function preserveDecimal(n){
-  return (parseFloat(n))
+  return parseFloat(n)
+}
+function preserveDecimal(string){
+  return parseFloat(string)
 }
