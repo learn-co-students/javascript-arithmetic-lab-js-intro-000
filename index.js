19136c21ead601efddfd972798d7c33269a33b9e
yvonneeeee <yvonnelin840701@gmail.com>
2021-03-08 16:18:21 +0000

    Done.

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ function divide(a,b){
 }
 
 function increment(n){
-  return (n += 1) //n++ || ++n
+  return (n += 1)
 }
 
 function decrement(n){
-  return (n -= 1) //n-- || --n
+  return (n -= 1)
 }
 
 function makeInt(n){
