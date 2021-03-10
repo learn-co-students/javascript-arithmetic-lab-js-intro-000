c697baf661cc900093e847c94f1b4985b6d5f0c5
Roxana Sulton <roxsulton@icloud.com>
2021-03-10 23:14:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,50 @@
+var number = 10
+
+
+function add() {
+  number += 5 // 10 + 5
+}
+
+function subtract() {
+  number -= 5 // 10 - 5
+}
+
+function multiply() {
+  number *= 5
+}
+
+function divide() {
+  numner /= 5
+}
+
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function increment(n) {
+return n+1
+}
+
+function decrement(n) {
+  return n-1
+}
+
+function makeInt(n) {
+  return  parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
