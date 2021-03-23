07f2fbaf5baa615bfde2984705bcf66aab9db2d3
Myles Starr <mylesstarr@gmail.com>
2021-03-23 13:33:37 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(x,y) {
+return x + y
+}
+
+function subtract(x,y) {
+return x - y
+}
+
+function multiply(x,y) {
+return x * y
+}
+
+function divide(x,y) {
+return x / y
+}
+
+function increment(n) {
+var number = n
+return n += 1
+}
+
+function decrement(n) {
+return n -= 1
+}
+
+function makeInt(string){
+return parseInt(string, 10)
+}
+
+function preserveDecimal(string){
+return parseFloat(string)
+}
