a4b5f44fd33cdcd3694f1eff03a89cf46d6271e6
Rose Martus <martus.rose@gmail.com>
2021-03-25 20:58:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+// Addition
+function add(x, y){
+  return x + y
+}
+
+//Subtraction
+function subtract(x, y){
+  return x - y
+}
+
+//multiplicatoin
+function multiply(x, y){
+  return x * y
+}
+
+//division
+function divide(x, y){
+  return x / y
+}
+
+//increment
+function increment(n){
+  return ++n
+}
+
+//decrement
+function decrement(n){
+  return --n
+}
+
+//makeInt
+function makeInt(n){
+  return parseInt(n, 10)
+}
+
+//preserveDecimal
+function preserveDecimal(n){
+  return parseFloat(n)
+}
