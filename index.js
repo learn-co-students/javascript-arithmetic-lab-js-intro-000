1e770f492f335da1f2387fbeeba1c42dc9dd8b3f
Daniel Williams <danielwil621@gmail.com>
2021-03-31 19:25:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a,b){
+  var sum = a+b;
+  return sum;
+}
+
+function subtract(a,b){
+  return a-b;
+}
+
+function multiply(a,b){
+  return a*b;
+}
+
+function divide(a,b){
+  return a/b;
+}
+
+function increment(n){
+  n = n+1;
+  return n;
+}
+
+function decrement(n){
+  n = n-1;
+  return n;
+}
+
+function makeInt(n){
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n){
+  return parseFloat(n);
+}
