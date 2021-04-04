3df4376cbed87f214fd79679f85ee985c59a4e29
bere abreo <berenicej2013@gmail.com>
2021-04-04 05:40:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+  return a+b;
+}
+
+function subtract(a, b) {
+  return a-b;
+}
+
+function multiply(a, b) {
+  return a*b;
+}
+
+function divide(a, b) {
+  return a/b;
+}
+
+function increment(n) {
+  return n +=
+}
+
+function decrement(n) {
+  return n -=
+}
+
+function makeInt(n) {
+  parseInt('n', 10)
+  return string;
+}
+
+function preserveDecimal(n) {
+  parseFloat('n');
+  return string;
+}
