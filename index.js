6c6f67c2e8bc99ba4727eb4b163ebc0d3ded8789
beautiful-inheritance-1081 <sduvly@gmail.com>
2021-04-04 02:53:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function add(a, b){
+return a += b
+
+}
+function subtract(a, b){
+return a -= b
+}
+function multiply(a, b){
+return a *= b
+}
+function divide(a, b){
+return a /= b
+}
+function increment(n){
+  return ++n
+}
+function decrement(n){
+  return --n
+}
+function makeInt(n, 10){
+  return n
+}
