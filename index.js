fc8839792e67fba45fcf70ada64208e2b1cab0b3
Joseph Autorino <staircasesoul@gmail.com>
2021-04-05 13:49:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a,b) {
+  return a - b
+}
+
+function divide(a,b) {
+  return a / b
+}
+
+function multiplly(a,b) {
+  return a * b
+}
+
+function increment(n) {
+  return n += 1
+}
+
+function decrement(n) {
+  return -= 1
+}
+
+function makeInt(string) {
+  return parseInt(string, 10)
+}
+
+function preserveDecimal(string) {
+  return parseFloat(string)
+}
