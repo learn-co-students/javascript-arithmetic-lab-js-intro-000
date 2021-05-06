dfecb1cb4a0d277d715dd3de950f7cbf4fb82574
Diego Domínguez Melo <diego_14melo@hotmail.com>
2021-05-06 04:50:55 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+const add = (a,b) => a + b
+const subtract = (a,b) => a - b
+const multiply = (a,b) => a*b
+const divide = (a,b) => a/b
+const increment = a => a + 1
+const decrement = a => a-1
+const makeInt = a => parseInt(a, 10)
+const preserveDecimal = a => parseFloat(a)
