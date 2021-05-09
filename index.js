0eb61656b9421f109c3f7a54781552f83a20102d
bright-generator-f905 <luzelenie1205@gmail.com>
2021-05-09 04:02:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function add (a,b)
+{return a+b}
+
+function subtract(a,b)
+{return a-b}
+
+function multiply(a,b)
+{return a*b}
+
+function divide (a,b)
+{return a/b}
+
+function increment(n)
+{return n+1}
+
+function decrement(n)
+{return n-1}
+
+function makeInt (n)
+{ var n = n.toString()
+return parseInt(n,10)
+}
+
+function preserveDecimal(n)
+{ var n = n.toString()
+return parseFloat(n,10)
+}
