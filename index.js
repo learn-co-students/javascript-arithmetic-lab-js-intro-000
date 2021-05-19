be47033420154c5bb340c4a97b601afe6da4424c
Nathan Huse <nathan.huse@yahoo.com>
2021-05-19 20:54:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(num1, num2) {
+  return num1+num2;
+}
+
+function subtract(num1, num2) {
+  return num1-num2;
+}
+
+function multiply(num1, num2) {
+  return num1*num2
+}
+
+function divide(num1, num2) {
+  return num1/num2;
+}
+
+function increment(num1) {
+  return ++num1;
+}
+
+function decrement(num1) {
+  return --num1;
+}
+
+function makeInt(num1) {
+  return parseInt(num1, 10);
+}
+
+function preserveDecimal(num1) {
+  return parseFloat(num1, 10);
+}
